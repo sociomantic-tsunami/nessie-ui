ca1ffc27fd6b7c25458fc8b8a30ac0a05c14138d
Damian Rodriguez <damian.rodriguez@sociomantic.com>
2017-12-14 16:24:13 +0100

    update for scroll indicators #124

--- src/ScrollBox/index.jsx
+++ src/ScrollBox/index.jsx
@@ -4,25 +4,58 @@ import PropTypes                from 'prop-types';
 import { buildClassName }       from '../utils';
 import styles                   from './scrollBox.css';
 import { createScrollHandler }  from './utils';
+import IconButton               from '../IconButton';
 
 const ScrollBox = ( {
-    cssMap,
     children,
     className,
     contentWidth,
+    cssMap,
     height,
+    onClickScrollDown,
+    onClickScrollLeft,
+    onClickScrollRight,
+    onClickScrollUp,
     onScroll,
-    scroll
+    scroll,
+    scrollBoxRef,
+    scrollDownIsVisible,
+    scrollLeftIsVisible,
+    scrollRightIsVisible,
+    scrollUpIsVisible
 } ) =>
-    <div
-        className = { buildClassName( className, cssMap, { scroll } ) }
-        onScroll  = { createScrollHandler( onScroll ) }
-        style     = { { maxHeight: height ? `${height}` : null } }>
+    <div className = { buildClassName( className, cssMap, { scroll } ) }>
+        { scrollDownIsVisible && <IconButton
+            className = { cssMap.icon__down }
+            iconType = "down"
+            iconSize = "L"
+            onClick = { onClickScrollDown } /> }
+        { scrollLeftIsVisible && <IconButton
+            className = { cssMap.icon__left }
+            iconType = "left"
+            iconSize = "L"
+            onClick = { onClickScrollLeft  } /> }
+        { scrollRightIsVisible && <IconButton
+            className = { cssMap.icon__right }
+            iconType = "right"
+            iconSize = "L"
+            onClick = { onClickScrollRight } /> }
+        { scrollUpIsVisible && <IconButton
+            className = { cssMap.icon__up }
+            iconType = "up"
+            iconSize = "L"
+            onClick = { onClickScrollUp } /> }
         <div
-            className = { cssMap.content }
-            style =
-                { { width: contentWidth ? `${contentWidth}%` : null  } }>
-            { children }
+            className = { cssMap.scrollBox }
+            onScroll  = { createScrollHandler( onScroll ) }
+            ref       = { scrollBoxRef }
+            style     = { { maxHeight: height ? `${height}` : null } }>
+            <div
+                className = { cssMap.content }
+                style =
+                    { { width: contentWidth ? `${contentWidth}%` : null  } }>
+                { children }
+            </div>
         </div>
     </div>;
 
@@ -31,33 +64,80 @@ ScrollBox.propTypes =
     /**
      *  ScrollBox content
      */
-    children     : PropTypes.node,
+    children           : PropTypes.node,
     /**
      *  ScrollBox content width, , specified in rem %
      */
-    contentWidth : PropTypes.number,
+    contentWidth       : PropTypes.number,
     /**
      *  ScrollBox height
      */
-    height       : PropTypes.string,
+    height             : PropTypes.string,
+    /**
+     *  on click scroll down icon callback function
+     */
+    onClickScrollDown  : PropTypes.func,
+    /**
+     *  on click scroll left icon callback function
+     */
+    onClickScrollLeft  : PropTypes.func,
+    /**
+     *  on click scroll right icon callback function
+     */
+    onClickScrollRight : PropTypes.func,
+    /**
+     *  on click scroll up icon callback function
+     */
+    onClickScrollUp    : PropTypes.func,
     /**
      *  on scroll callback function
      */
-    onScroll     : PropTypes.func,
+    onScroll           : PropTypes.func,
     /**
      *  Scroll direction
      */
-    scroll       : PropTypes.oneOf( [ 'horizontal', 'vertical', 'both' ] )
-
+    scroll             : PropTypes.oneOf( [
+        'horizontal',
+        'vertical',
+        'both'
+    ] ),
+    /**
+     * Callback ref: ( ref ) => { ... }
+     */
+    scrollBoxRef         : PropTypes.func,
+    /**
+     *  Display Scroll left icon
+     */
+    scrollLeftIsVisible  : PropTypes.bool,
+    /**
+     *  Display Scroll right icon
+     */
+    scrollRightIsVisible : PropTypes.bool,
+    /**
+     *  Display Scroll up icon
+     */
+    scrollUpIsVisible    : PropTypes.bool,
+    /**
+     *  Display Scroll down icon
+     */
+    scrollDownIsVisible  : PropTypes.bool
 };
 
 ScrollBox.defaultProps =
 {
-    children : undefined,
-    cssMap   : styles,
-    height   : undefined,
-    onScroll : undefined,
-    scroll   : 'both',
+    children             : undefined,
+    cssMap               : styles,
+    height               : undefined,
+    onClickScrollDown    : undefined,
+    onClickScrollLeft    : undefined,
+    onClickScrollRight   : undefined,
+    onClickScrollUp      : undefined,
+    onScroll             : undefined,
+    scroll               : 'both',
+    scrollDownIsVisible  : false,
+    scrollLeftIsVisible  : false,
+    scrollRightIsVisible : false,
+    scrollUpIsVisible    : false
 };
 
 export default ScrollBox;
