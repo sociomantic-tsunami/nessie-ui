0d67f8b18a0893a060b4cb505938909b7c80ce5a
Conor Cafferkey <conor.cafferkey@sociomantic.com>
2018-02-27 12:15:28 +0100

    Simplify TextInputWithDropdown

--- src/TextInputWithDropdown/index.jsx
+++ src/TextInputWithDropdown/index.jsx
@@ -1,289 +1,252 @@
-import React                            from 'react';
-import PropTypes                        from 'prop-types';
+import React                          from 'react';
+import PropTypes                      from 'prop-types';
 
-import { InputField, FlounderDropdown } from '../index';
-import Component                        from '../proto/Component';
-import InputContainer                   from '../proto/InputContainer';
-import Css                              from '../hoc/Css';
+import {
+    Column,
+    FlounderDropdown,
+    InputField,
+    Row,
+} from '../index';
+import { buildClassName, generateId } from '../utils';
+import styles                         from './textInputWithDropdown.css';
+import InputContainer                 from '../proto/InputContainer';
 
 
-export default class TextInputWithDropdown extends Component
+const TextInputWithDropdown = ( {
+    className,
+    cssMap,
+    dropdownData,
+    dropdownDefaultValue,
+    dropdownPlaceholder,
+    dropdownPosition,
+    dropdownValue,
+    errorMessage,
+    errorMessageIsVisible,
+    errorMessagePosition,
+    forceHover,
+    hasError,
+    id,
+    inputDefaultValue,
+    inputPlaceholder,
+    inputRef,
+    inputValue,
+    isDisabled,
+    isReadOnly,
+    label,
+    labelPosition,
+    name,
+    onBlur,
+    onChange,
+    onFocus,
+    onMouseOver,
+    onMouseOut,
+    textAlign
+} ) =>
 {
-    static propTypes =
+    let alignText = textAlign;
+
+    if ( textAlign === 'auto' )
     {
-        /**
-         *  Label text string or JSX node
-         */
-        label         : PropTypes.node,
-        /**
-         *  Label position
-         */
-        labelPosition : PropTypes.oneOf( [
-            'top',
-            'left',
-            'right'
-        ] ),
-        /**
-         *  Input placeholder text
-         */
-        inputPlaceholder      : PropTypes.string,
-        /**
-         *  Dropdown placeholder text
-         */
-        dropdownPlaceholder   : PropTypes.string,
-        /**
-         *  Display as disabled
-         */
-        isDisabled            : PropTypes.bool,
-        /**
-         *  Display as read-only
-         */
-        isReadOnly            : PropTypes.bool,
-        /**
-         *  Display as error/invalid
-         */
-        hasError              : PropTypes.bool,
-        /**
-         *  Tooltip message text (string or JSX)
-         */
-        errorMessage          : PropTypes.node,
-        /**
-         *  Error message is displayed
-         */
-        errorMessageIsVisible : PropTypes.bool,
-        /**
-         *  Error message position relative to the icon
-         */
-        errorMessagePosition  : PropTypes.oneOf( [
-            'top',
-            'topLeft'
-        ] ),
-        /**
-            *  Array of strings or objects to build the dropdown
-            */
-        dropdownData : PropTypes.oneOfType( [
-            PropTypes.arrayOf(
-                PropTypes.shape( {
-                    text        : PropTypes.string,
-                    value       : PropTypes.string,
-                    description : PropTypes.string,
-                    disabled    : PropTypes.bool,
-                    extraClass  : PropTypes.string,
-                    icon        : PropTypes.string
-                } )
-            ),
-            PropTypes.arrayOf( PropTypes.string )
-        ] ),
-        /**
-         * Initial input string value
-         */
-        inputDefaultValue    : PropTypes.string,
-        /**
-         * Input string value
-         */
-        inputValue           : PropTypes.string,
-        /**
-         * Initial dropdown selected value
-         */
-        dropdownDefaultValue : PropTypes.string,
-        /**
-         * Dropdown selected value
-         */
-        dropdownValue        : PropTypes.string,
-        /**
-         * Position of the dropdown
-         */
-        dropdownPosition     : PropTypes.oneOf( [ 'left', 'right' ] ),
-        /**
-         * Input text alignment
-         */
-        textAlign  : PropTypes.oneOf( [ 'auto', 'left', 'right' ] ),
-        /**
-         * HTML id attribute (overwrite default)
-         */
-        id         : PropTypes.string,
-         /**
-         * HTML name attribute
-         */
-        name       : PropTypes.string,
-        /**
-         *  onChange callback function: ( e ) => { ... }
-         */
-        onChange   : PropTypes.func,
-        /**
-         *  onFocus callback function: ( e ) => { ... }
-         */
-        onFocus    : PropTypes.func,
-        /**
-         *  onBlur callback function: ( e ) => { ... }
-         */
-        onBlur     : PropTypes.func,
-        /**
-         *  onMouseOver callback function: ( e ) => { ... }
-         */
-        onMouseOve : PropTypes.func,
-        /**
-         *  onMouseOut callback function: ( e ) => { ... }
-         */
-        onMouseOut : PropTypes.func,
-        /**
-         * Display as hover when required from another component
-         */
-        forceHover : PropTypes.bool,
-        /**
-         * Callback that receives the native <input>: ( ref ) => { ... }
-         */
-        inputRef   : PropTypes.func,
-    };
-
-    static defaultProps =
-    {
-        labelPosition         : 'top',
-        isDisabled            : false,
-        isReadOnly            : false,
-        hasError              : false,
-        errorMessageIsVisible : false,
-        errorMessagePosition  : 'top',
-        dropdownPosition      : 'right',
-        textAlign             : 'auto',
-        forceHover            : false,
-        cssMap                : require( './textInputWithDropdown.css' ),
-    };
-
-    constructor( props )
-    {
-        super( props );
-
-        this.state = {
-            ...this.state,
-            isFocused : false,
-            isHovered : false
-        };
-
-        this.handleFocus     = this.handleFocus.bind( this );
-        this.handleBlur      = this.handleBlur.bind( this );
-        this.handleMouseOver = this.handleMouseOver.bind( this );
-        this.handleMouseOut  = this.handleMouseOut.bind( this );
+        alignText = dropdownPosition === 'left' ? 'right' : 'left';
     }
 
-    handleFocus( e )
-    {
-        this.setState( { isFocused: true  } );
+    return (
+        <InputContainer
+            className = { buildClassName( className, cssMap, {
+                position : dropdownPosition
+            } ) }
+            errorMessage          = { errorMessage }
+            errorMessageIsVisible = { errorMessageIsVisible }
+            errorMessagePosition  = { errorMessagePosition }
+            hasError              = { hasError }
+            id                    = { id }
+            isDisabled            = { isDisabled }
+            label                 = { label }
+            labelPosition         = { labelPosition }
+            onMouseOut            = { onMouseOut }
+            onMouseOver           = { onMouseOver }>
+            <Row
+                className    = { cssMap.row }
+                onMouseOut   = { onMouseOut }
+                onMouseOver  = { onMouseOver }
+                verticalAlign = "middle">
+                <Column>
+                    <InputField
+                        hasError     = { hasError }
+                        id           = { id }
+                        inputRef     = { inputRef }
+                        isDisabled   = { isDisabled }
+                        isReadOnly   = { isReadOnly }
+                        placeholder  = { inputPlaceholder }
+                        defaultValue = { inputDefaultValue }
+                        forceHover   = { forceHover }
+                        onBlur       = { onBlur }
+                        onChange     = { onChange }
+                        onFocus      = { onFocus }
+                        name         = { name }
+                        textAlign    = { alignText }
+                        value        = { inputValue } />
+                </Column>
+                <Column size = "content">
+                    <FlounderDropdown
+                        data         = { dropdownData }
+                        hasError     = { hasError }
+                        isDisabled   = { isDisabled }
+                        isReadOnly   = { isReadOnly }
+                        placeholder  = { dropdownPlaceholder }
+                        defaultValue = { dropdownDefaultValue }
+                        value        = { dropdownValue }
+                        forceHover   = { forceHover }
+                        onBlur       = { onBlur }
+                        onChange     = { onChange }
+                        onFocus      = { onFocus } />
+                </Column>
+            </Row>
+        </InputContainer>
+    );
+};
 
-        const { onFocus } = this.props;
-        if ( onFocus )
-        {
-            onFocus( e );
-        }
-    }
-    handleBlur( e )
-    {
-        this.setState( { isFocused: false  } );
-        const { onBlur } = this.props;
-        if ( onBlur )
-        {
-            onBlur( e );
-        }
-    }
-    handleMouseOver( e )
-    {
-        this.setState( { isHovered: true  } );
 
-        const { onMouseOver } = this.props;
-        if ( onMouseOver )
-        {
-            onMouseOver( e );
-        }
-    }
-    handleMouseOut( e )
-    {
-        this.setState( { isHovered: false } );
+TextInputWithDropdown.propTypes =
+{
+    /**
+     *  Array of strings or objects to build the dropdown
+     */
+    dropdownData : PropTypes.oneOfType( [
+        PropTypes.arrayOf( PropTypes.object ),
+        PropTypes.arrayOf( PropTypes.string ),
+    ] ),
+    /**
+     * Initial dropdown selected value
+     */
+    dropdownDefaultValue  : PropTypes.string,
+    /**
+     *  Dropdown placeholder text
+     */
+    dropdownPlaceholder   : PropTypes.string,
+    /**
+     * Position of the dropdown
+     */
+    dropdownPosition      : PropTypes.oneOf( [ 'left', 'right' ] ),
+    /**
+     * Dropdown selected value
+     */
+    dropdownValue         : PropTypes.string,
+    /**
+     *  Tooltip message text (string or JSX)
+     */
+    errorMessage          : PropTypes.node,
+    /**
+     *  Error message is displayed
+     */
+    errorMessageIsVisible : PropTypes.bool,
+    /**
+     *  Error message position relative to the icon
+     */
+    errorMessagePosition  : PropTypes.oneOf( [ 'top', 'topLeft' ] ),
+    /**
+     * Display as hover when required from another component
+     */
+    forceHover            : PropTypes.bool,
+    /**
+     *  Display as error/invalid
+     */
+    hasError              : PropTypes.bool,
+    /**
+     * HTML id attribute (overwrite default)
+     */
+    id                    : PropTypes.string,
+    /**
+     * Initial input string value
+     */
+    inputDefaultValue     : PropTypes.string,
+    /**
+     *  Input placeholder text
+     */
+    inputPlaceholder      : PropTypes.string,
+    /**
+     * Callback that receives the native <input>: ( ref ) => { ... }
+     */
+    inputRef              : PropTypes.func,
+    /**
+     * Input string value
+     */
+    inputValue            : PropTypes.string,
+    /**
+     *  Display as disabled
+     */
+    isDisabled            : PropTypes.bool,
+    /**
+     *  Display as read-only
+     */
+    isReadOnly            : PropTypes.bool,
+    /**
+     *  Label text string or JSX node
+     */
+    label                 : PropTypes.node,
+    /**
+     *  Label position
+     */
+    labelPosition         : PropTypes.oneOf( [ 'top', 'left', 'right' ] ),
+    /**
+     * HTML name attribute
+     */
+    name                  : PropTypes.string,
+    /**
+     *  onBlur callback function: ( e ) => { ... }
+     */
+    onBlur                : PropTypes.func,
+    /**
+     *  onChange callback function: ( e ) => { ... }
+     */
+    onChange              : PropTypes.func,
+    /**
+     *  onFocus callback function: ( e ) => { ... }
+     */
+    onFocus               : PropTypes.func,
+    /**
+     *  onMouseOut callback function: ( e ) => { ... }
+     */
+    onMouseOut            : PropTypes.func,
+    /**
+     *  onMouseOver callback function: ( e ) => { ... }
+     */
+    onMouseOver           : PropTypes.func,
+    /**
+     * Input text alignment
+     */
+    textAlign             : PropTypes.oneOf( [ 'auto', 'left', 'right' ] ),
+};
 
-        const { onMouseOut } = this.props;
-        if ( onMouseOut )
-        {
-            onMouseOut( e );
-        }
-    }
+TextInputWithDropdown.defaultProps =
+{
+    cssMap                : styles,
+    dropdownData          : undefined,
+    dropdownDefaultValue  : undefined,
+    dropdownPlaceholder   : undefined,
+    dropdownPosition      : 'right',
+    dropdownValue         : undefined,
+    errorMessage          : undefined,
+    errorMessageIsVisible : false,
+    errorMessagePosition  : 'top',
+    forceHover            : false,
+    hasError              : false,
+    id                    : generateId( 'TextInputWithDropdown' ),
+    inputDefaultValue     : undefined,
+    inputPlaceholder      : undefined,
+    inputRef              : undefined,
+    inputValue            : undefined,
+    isDisabled            : false,
+    isReadOnly            : false,
+    label                 : undefined,
+    labelPosition         : 'top',
+    name                  : undefined,
+    onBlur                : undefined,
+    onChange              : undefined,
+    onFocus               : undefined,
+    onMouseOut            : undefined,
+    onMouseOver           : undefined,
+    textAlign             : 'auto',
+};
 
-    render()
-    {
-        const { className, cssMap, label, ...props } = this.props;
-
-        const {
-            dropdownData,
-            dropdownDefaultValue,
-            dropdownPlaceholder,
-            dropdownPosition,
-            dropdownValue,
-            forceHover,
-            hasError,
-            inputDefaultValue,
-            inputPlaceholder,
-            inputValue,
-            isDisabled,
-            labelPosition,
-            textAlign
-        } = props;
-
-        const {
-            id,
-            isFocused,
-            isHovered
-        } = this.state;
-
-        let alignText = textAlign;
-
-        if ( textAlign === 'auto' )
-        {
-            alignText = dropdownPosition === 'left' ? 'right' : 'left';
-        }
-
-        const fakeHovered = forceHover || isFocused || isHovered;
-
-        return (
-            <Css
-                cssMap   = { cssMap }
-                cssProps = { {
-                    labelPosition,
-                    disabled : isDisabled,
-                    position : dropdownPosition,
-                    error    : hasError
-                } }>
-                <InputContainer
-                    { ...props }
-                    id        = { id }
-                    className = { className }
-                    label     = { label }>
-                    <div className = { cssMap.container }>
-                        <InputField
-                            { ...props }
-                            className    = { cssMap.input }
-                            id           = { id }
-                            placeholder  = { inputPlaceholder }
-                            defaultValue = { inputDefaultValue }
-                            value        = { inputValue }
-                            textAlign    = { alignText }
-                            forceHover   = { fakeHovered }
-                            onMouseOver  = { this.handleMouseOver }
-                            onMouseOut   = { this.handleMouseOut }
-                            onFocus      = { this.handleFocus }
-                            onBlur       = { this.handleBlur } />
-                        <FlounderDropdown
-                            { ...props }
-                            className    = { cssMap.dropdown }
-                            data         = { dropdownData }
-                            placeholder  = { dropdownPlaceholder }
-                            defaultValue = { dropdownDefaultValue }
-                            value        = { dropdownValue }
-                            forceHover   = { fakeHovered }
-                            onMouseOver  = { this.handleMouseOver }
-                            onMouseOut   = { this.handleMouseOut }
-                            onFocus      = { this.handleFocus }
-                            onBlur       = { this.handleBlur } />
-                    </div>
-                </InputContainer>
-            </Css>
-        );
-    }
-}
+export default TextInputWithDropdown;
