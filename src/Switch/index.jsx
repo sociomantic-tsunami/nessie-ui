babebc01dbdd4d65becb5ec21f8918c4fd1a9a7f
Conor Cafferkey <conor.cafferkey@sociomantic.com>
2018-03-01 11:56:18 +0100

    Some Switch tweaks #207

--- src/Switch/index.jsx
+++ src/Switch/index.jsx
@@ -1,118 +1,129 @@
-import React        from 'react';
-import PropTypes    from 'prop-types';
+import React                          from 'react';
+import PropTypes                      from 'prop-types';
 
-import Component    from '../proto/Component';
-import Css          from '../hoc/Css';
+import { buildClassName, generateId } from '../utils';
+import styles                         from './switch.css';
 
-export default class Switch extends Component
+
+const Switch = ( {
+    className,
+    cssMap,
+    forceHover,
+    id,
+    isDefaultChecked,
+    isChecked,
+    isDisabled,
+    isReadOnly,
+    name,
+    onChange,
+    onLabel,
+    offLabel,
+    showLabel,
+    value,
+} ) => (
+    <div
+        className = { buildClassName( className, cssMap, {
+            disabled    : isDisabled,
+            fakeHovered : !isDisabled && forceHover
+        } ) }>
+        <input
+            className      = { cssMap.input }
+            defaultChecked = { isDefaultChecked }
+            defaultValue   = { value }
+            disabled       = { isDisabled || isReadOnly }
+            checked        = { isChecked }
+            id             = { id }
+            name           = { name }
+            onChange       = { !isReadOnly && onChange }
+            type           = "checkbox" />
+        <label
+            className = { cssMap.label }
+            htmlFor   = { id }>
+            <div className = { cssMap.on }>
+                { showLabel && onLabel }
+            </div>
+            <div className = { cssMap.off }>
+                { showLabel && offLabel }
+            </div>
+        </label>
+    </div>
+);
+
+Switch.propTypes =
 {
-    static propTypes =
-    {
-        /**
-         *  “On”/checked label text
-         */
-        onLabel    : PropTypes.string,
-        /**
-         *  “Off”/unchecked label text
-         */
-        offLabel   : PropTypes.string,
-        /**
-         *  input name
-         */
-        name       : PropTypes.string,
-        /**
-         *  Show “on/off” label text
-         */
-        showLabel  : PropTypes.bool,
-        /**
-         *  Display as checked/“on”
-         */
-        isChecked  : PropTypes.bool,
-        /**
-         *  Display as disabled
-         */
-        isDisabled : PropTypes.bool,
-        /**
-         *  Display as read-only
-         */
-        isReadOnly : PropTypes.bool,
-        /**
-         * Display as hover when required from another component
-         */
-        forceHover : PropTypes.bool,
-        /**
-         * HTML value attribute
-         */
-        value      : PropTypes.string,
-        /**
-         * HTML id attribute (overrides default)
-         */
-        id         : PropTypes.string,
-        /**
-         * onChange callback function: ( e ) => { ... }
-         */
-        onChange   : PropTypes.func
-    };
+    /**
+     *  Extra CSS class name
+     */
+    className        : PropTypes.string,
+    /**
+     *  CSS class map
+     */
+    cssMap           : PropTypes.objectOf( PropTypes.string ),
+    /**
+     * Display as hover when required from another component
+     */
+    forceHover       : PropTypes.bool,
+    /**
+     * HTML id attribute (overrides default)
+     */
+    id               : PropTypes.string,
+    /**
+     *  Display as checked/“on” by default (uncontrolled input)
+     */
+    isDefaultChecked : PropTypes.bool,
+    /**
+     *  Display as checked/“on” (controlled input)
+     */
+    isChecked        : PropTypes.bool,
+    /**
+     *  Display as disabled
+     */
+    isDisabled       : PropTypes.bool,
+    /**
+     *  Display as read-only
+     */
+    isReadOnly       : PropTypes.bool,
+    /**
+     * onChange callback function: ( e ) => { ... }
+     */
+    onChange         : PropTypes.func,
+    /**
+     *  “On”/checked label text
+     */
+    onLabel          : PropTypes.string,
+    /**
+     *  “Off”/unchecked label text
+     */
+    offLabel         : PropTypes.string,
+    /**
+     *  HTML name attribute for input
+     */
+    name             : PropTypes.string,
+    /**
+     *  Show “on/off” label text
+     */
+    showLabel        : PropTypes.bool,
+    /**
+     * HTML value attribute for input
+     */
+    value            : PropTypes.string,
+};
 
-    static defaultProps =
-    {
-        onLabel    : 'On',
-        offLabel   : 'Off',
-        showLabel  : true,
-        isChecked  : false,
-        isDisabled : false,
-        isReadOnly : false,
-        forceHover : false,
-        cssMap     : require( './switch.css' )
-    };
+Switch.defaultProps =
+{
+    className        : undefined,
+    cssMap           : styles,
+    forceHover       : false,
+    id               : generateId( 'Switch' ),
+    isDefaultChecked : false,
+    isChecked        : undefined,
+    isDisabled       : false,
+    isReadOnly       : false,
+    onChange         : undefined,
+    onLabel          : 'On',
+    offLabel         : 'Off',
+    showLabel        : true,
+    value            : undefined,
+};
 
-    render()
-    {
-        const {
-            className,
-            cssMap,
-            forceHover,
-            onChange,
-            isChecked,
-            isDisabled,
-            isReadOnly,
-            name,
-            onLabel,
-            offLabel,
-            showLabel,
-            value } = this.props;
-
-        const { id } = this.state;
-
-        return (
-            <Css
-                cssMap   = { cssMap }
-                cssProps = { {
-                    disabled    : isDisabled,
-                    fakeHovered : !isDisabled && forceHover
-                } }>
-                <div className = { className }>
-                    <input
-                        type         = "checkbox"
-                        name         = { name }
-                        id           = { id }
-                        className    = { cssMap.input }
-                        defaultValue = { value }
-                        onChange     = { !isReadOnly && onChange }
-                        checked      = { isChecked }
-                        disabled     = { isDisabled || isReadOnly } />
-                    <label
-                        htmlFor   = { id }
-                        className = { cssMap.label }>
-                        <div className = { cssMap.on }>
-                            { showLabel && onLabel }
-                        </div>
-                        <div className = { cssMap.off }>
-                            { showLabel && offLabel }
-                        </div>
-                    </label>
-                </div>
-            </Css>
-        );
-    }
-}
+export default Switch;
