4e883e2777f16548a1caaa79d6d2b8903abee0a6
Damian Rodriguez <damian.rodriguez@sociomantic.com>
2018-03-06 14:22:17 +0100

    add basic tests for AccordionItem component #114

---
+++ src/AccordionItem/tests.jsx
@@ -0,0 +1,98 @@
+/* eslint-env node, mocha */
+/* eslint-disable no-magic-numbers, no-multi-str, no-unused-expressions */
+/* global expect */
+
+import React                from 'react';
+import { shallow }          from 'enzyme';
+
+import H2                   from '../H2';
+import Icon                 from '../Icon';
+import AccordionItemHeader  from './AccordionItemHeader';
+import AccordionItemContent from './AccordionItemContent';
+
+import AccordionItem        from './index';
+
+describe( 'AccordionItem', () =>
+{
+    let wrapper;
+    // let instance;
+
+    beforeEach( () =>
+    {
+        wrapper  = shallow( <AccordionItem /> );
+        // instance = wrapper.instance();
+    } );
+
+    describe( 'render()', () =>
+    {
+        it( 'should contain exactly one Accordion Item Header', () =>
+        {
+            expect( wrapper.find( AccordionItemHeader ) ).to.have.length( 1 );
+        } );
+
+        it( 'should contain an Accordion Item Content when is expanded', () =>
+        {
+            wrapper.setProps( { isOpen: true } );
+            expect( wrapper.find( AccordionItemContent ) ).to.have.length( 1 );
+        } );
+
+        it( 'should not contain an Accordion Item Content when collapsed', () =>
+        {
+            wrapper.setProps( { isOpen: false } );
+            expect( wrapper.find( AccordionItemContent ) ).to.have.length( 0 );
+        } );
+    } );
+} );
+
+describe( 'AccordionItemHeader', () =>
+{
+    let wrapper;
+    // let instance;
+
+    beforeEach( () =>
+    {
+        wrapper  = shallow( <AccordionItemHeader /> );
+        // instance = wrapper.instance();
+    } );
+
+    describe( 'render()', () =>
+    {
+        it( 'should contain exactly one <button>', () =>
+        {
+            expect( wrapper.find( 'button' ) ).to.have.length( 1 );
+        } );
+
+        it( 'should have a exactly one Icon', () =>
+        {
+            expect( wrapper.find( Icon ) ).to.have.length( 1 );
+        } );
+
+        it( 'should contain a H2 component by default', () =>
+        {
+            wrapper.setProps( { headerText: 'Boom' } );
+            expect( wrapper.find( H2 ) ).to.have.length( 1 );
+        } );
+    } );
+
+    describe( 'props', () =>
+    {
+        describe( 'iconType', () =>
+        {
+            it( 'should contain "down" icon when is collapsed', () =>
+            {
+                wrapper.setProps( { isOpen: false } );
+
+                expect( wrapper.find( Icon ).prop( 'type' ) )
+                    .to.equal( 'down' );
+            } );
+
+            it( 'should contain "up" icon when is expanded', () =>
+            {
+                wrapper.setProps( { isOpen: true } );
+
+                expect( wrapper.find( Icon ).prop( 'type' ) )
+                    .to.equal( 'up' );
+            } );
+        } );
+    } );
+} );
