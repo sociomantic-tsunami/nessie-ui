8fd3e2f533cbf40104eb21f0c7e1ff37bf87324b
daniel-martic-sociomantic <daniel.martic@sociomantic.com>
2018-03-26 16:45:35 +0200

    Added tests for Required #287

---
+++ src/Required/tests.jsx
@@ -0,0 +1,54 @@
+/* eslint-env node, mocha */
+/* eslint no-console: 0 */
+/* eslint-disable no-magic-numbers, no-multi-str, no-unused-expressions */
+
+import React        from 'react';
+import { mount }    from 'enzyme';
+
+import { Required } from '../index';
+
+describe.only( 'Required', () =>
+{
+    let wrapper;
+
+    beforeEach( () =>
+    {
+        wrapper  = mount( <Required /> );
+    } );
+
+    describe( 'render()', () =>
+    {
+        it( 'should contain exactly one Required', () =>
+        {
+            expect( wrapper ).to.have.length( 1 );
+        } );
+    } );
+
+    describe( 'props', () =>
+    {
+        describe( 'isRequired', () =>
+        {
+            it( 'should be "true" by default', () =>
+            {
+                expect( wrapper.prop( 'isRequired' ) ).to.be.true;
+            } );
+
+            it( 'should be passed to the Required', () =>
+            {
+                wrapper.setProps( { isRequired: false } );
+
+                expect( wrapper.prop( 'isRequired' ) ).to.be.false;
+            } );
+        } );
+
+        describe( 'text', () =>
+        {
+            it( 'should be passed to the Required', () =>
+            {
+                wrapper.setProps( { text: 'Nanananananana' } );
+
+                expect( wrapper.prop( 'text' ) ).to.equal( 'Nanananananana' );
+            } );
+        } );
+    } );
+} );
