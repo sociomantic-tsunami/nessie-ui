d3dc84c349506479eabb079513925116563bf852
daniel-martic-sociomantic <daniel.martic@sociomantic.com>
2018-04-18 14:46:55 +0200

    Added onClick and onMouse driver methods #298

--- src/ListBox/driver.js
+++ src/ListBox/driver.js
@@ -1,7 +1,35 @@
+// const ERRORS = {
+//     OPTION_CANNOT_BE_CLICKED : () => `Option cannot be clicked since it's disabled`, // eslint-disable-line max-len
+// };
+
 export default class ListBoxDriver
 {
     constructor( wrapper )
     {
         this.wrapper = wrapper;
+    }
+
+    clickOption( index = 0 )
+    {
+        // if ( this.props.disabled )
+        // {
+        //     throw new Error(
+        //         ERRORS.OPTION_CANNOT_BE_CLICKED()
+        //     );
+        // }
+
+        this.wrapper.find( 'ListBoxOption' ).at( index ).simulate( 'click' );
+    }
+
+    mouseOverOption( index = 0 )
+    {
+        this.wrapper.find( 'ListBoxOption' ).at( index )
+            .simulate( 'mouseenter' );
+    }
+
+    mouseOutOption( index = 0 )
+    {
+        this.wrapper.find( 'ListBoxOption' ).at( index )
+            .simulate( 'mouseleave' );
     }
 }
--- src/ListBox/tests.jsx
+++ src/ListBox/tests.jsx
@@ -3,10 +3,10 @@
 /* eslint no-console: 0*/
 /* eslint-disable no-magic-numbers, no-multi-str*/
 
-import React          from 'react';
-import { shallow }    from 'enzyme';
+import React                 from 'react';
+import { shallow, mount }    from 'enzyme';
 
-import ListBox        from './index';
+import ListBox               from './index';
 
 
 describe( 'ListBox', () =>
@@ -31,5 +31,158 @@ describe( 'ListBox', () =>
             wrapper.setProps( { children: [ <ListBox />, <ListBox /> ] } );
             expect( wrapper.find( ListBox ) ).to.have.length( 2 );
         } );
+    } );
+} );
+
+
+describe.only( 'ListBoxDriver', () =>
+{
+    let wrapper;
+    let driver;
+
+    beforeEach( () =>
+    {
+        wrapper  = mount( <ListBox /> );
+        driver   = wrapper.driver();
+    } );
+
+    describe( 'clickOption', () =>
+    {
+        it( 'should trigger onClickOption when clicked on \
+ListBoxOption at given index', () =>
+            {
+                const onClickOption = sinon.spy();
+
+                wrapper.setProps( {
+                    onClickOption,
+                    options : [ {
+                        'text' : 'Option'
+                    },
+                    {
+                        'text'        : 'Option with description',
+                        'value'       : 'value2',
+                        'description' : 'Option description'
+                    },
+                    {
+                        'text'     : 'Disabled option',
+                        'value'    : 'value3',
+                        'disabled' : true
+                    },
+                    {
+                        header  : 'Subsection 1',
+                        options : [
+                            {
+                                'text' : 'Subsection option 1'
+                            },
+                            {
+                                'text' : 'Subsection option 2'
+                            },
+                            {
+                                'text'        : 'Subsection description',
+                                'description' : 'Option description',
+                                'value'       : 'value12'
+                            }
+                        ]
+                    } ]
+                } );
+
+                driver.clickOption( 2 );
+
+                expect( onClickOption.calledOnce ).to.be.true;
+            } );
+    } );
+
+
+    describe( 'mouseOverOption', () =>
+    {
+        it( 'should trigger onMouseOverOption when hovered on \
+ListBoxOption at given index', () =>
+            {
+                const onMouseOverOption = sinon.spy();
+
+                wrapper.setProps( {
+                    onMouseOverOption,
+                    options : [ {
+                        'text' : 'Option'
+                    },
+                    {
+                        'text'        : 'Option with description',
+                        'value'       : 'value2',
+                        'description' : 'Option description'
+                    },
+                    {
+                        'text'     : 'Disabled option',
+                        'value'    : 'value3',
+                        'disabled' : true
+                    },
+                    {
+                        header  : 'Subsection 1',
+                        options : [
+                            {
+                                'text' : 'Subsection option 1'
+                            },
+                            {
+                                'text' : 'Subsection option 2'
+                            },
+                            {
+                                'text'        : 'Subsection description',
+                                'description' : 'Option description',
+                                'value'       : 'value12'
+                            }
+                        ]
+                    } ]
+                } );
+
+                driver.mouseOverOption( 1 );
+
+                expect( onMouseOverOption.calledOnce ).to.be.true;
+            } );
+    } );
+
+
+    describe( 'mouseOutOption', () =>
+    {
+        it( 'should trigger onMouseOutOption when hovered on \
+ListBoxOption at given index', () =>
+            {
+                const onMouseOutOption = sinon.spy();
+
+                wrapper.setProps( {
+                    onMouseOutOption,
+                    options : [ {
+                        'text' : 'Option'
+                    },
+                    {
+                        'text'        : 'Option with description',
+                        'value'       : 'value2',
+                        'description' : 'Option description'
+                    },
+                    {
+                        'text'     : 'Disabled option',
+                        'value'    : 'value3',
+                        'disabled' : true
+                    },
+                    {
+                        header  : 'Subsection 1',
+                        options : [
+                            {
+                                'text' : 'Subsection option 1'
+                            },
+                            {
+                                'text' : 'Subsection option 2'
+                            },
+                            {
+                                'text'        : 'Subsection description',
+                                'description' : 'Option description',
+                                'value'       : 'value12'
+                            }
+                        ]
+                    } ]
+                } );
+
+                driver.mouseOutOption( 0 );
+
+                expect( onMouseOutOption.calledOnce ).to.be.true;
+            } );
     } );
 } );
