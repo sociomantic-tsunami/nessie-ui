7270483bf1996caf445b4d99e89c5ffbbbfb5cea
daniel-martic-sociomantic <daniel.martic@sociomantic.com>
2018-04-19 12:50:28 +0200

    Added clickItem, clickNext and clickPrev #288

--- src/DatePicker/driver.js
+++ src/DatePicker/driver.js
@@ -5,4 +5,25 @@ export default class DatePickerDriver
         this.wrapper = wrapper;
         this.cssMap  = this.wrapper.props().cssMap;
     }
+
+    clickItem( index = 0 )
+    {
+        this.wrapper.find( '.datePickerItem__default' ).at( index )
+            .simulate( 'click' );
+        return this;
+    }
+
+    clickNext()
+    {
+        this.wrapper.find( '.datePickerHeader__next' )
+            .simulate( 'click' );
+        return this;
+    }
+
+    clickPrev()
+    {
+        this.wrapper.find( '.datePickerHeader__prev' )
+            .simulate( 'click' );
+        return this;
+    }
 }
