ad33635f23b79dccdefadf8d30d271fac735c91c
daniel-martic-sociomantic <daniel.martic@sociomantic.com>
2018-05-28 13:54:07 +0200

    Added minimizers for js and css #83

--- cfg/dist.js
+++ cfg/dist.js
@@ -1,12 +1,14 @@
-const path                 = require( 'path' );
+const path                    = require( 'path' );
 
-const webpack              = require( 'webpack' );
-const HappyPack            = require( 'happypack' );
-const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
-const cloneDeep            = require( 'lodash.clonedeep' );
+const webpack                 = require( 'webpack' );
+const HappyPack               = require( 'happypack' );
+const UglifyJsPlugin          = require( 'uglifyjs-webpack-plugin' );
+const MiniCssExtractPlugin    = require( 'mini-css-extract-plugin' );
+const OptimizeCSSAssetsPlugin = require( 'optimize-css-assets-webpack-plugin' );
+const cloneDeep               = require( 'lodash.clonedeep' );
 
-const baseConfig           = require( './base' );
-const defaultSettings      = require( './defaults' );
+const baseConfig              = require( './base' );
+const defaultSettings         = require( './defaults' );
 
 
 const commonDistPLugins = [
@@ -78,6 +80,18 @@ const commonJsConfig = Object.assign( {}, baseConfig, {
             commonjs  : 'react-dom',
             amd       : 'react-dom'
         }
+    },
+
+    optimization :
+    {
+        minimizer : [
+            new UglifyJsPlugin( {
+                cache     : true,
+                parallel  : true,
+                sourceMap : true // set to true if you want JS source maps
+            } ),
+            new OptimizeCSSAssetsPlugin( {} )
+        ]
     },
 
     plugins : commonDistPLugins.concat( [
