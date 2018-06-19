5807900b28198035448b3eaa706bd81f7c61d4c0
Conor Cafferkey <conor.cafferkey@sociomantic.com>
2018-06-19 11:02:21 +0200

    Add MessageBox “default” styling

--- src/MessageBox/index.jsx
+++ src/MessageBox/index.jsx
@@ -35,12 +35,18 @@ MessageBox.propTypes =
     /**
     *  Message type
     */
-    messageType : PropTypes.oneOf( [ 'alert', 'info', 'error', 'success' ] )
+    messageType : PropTypes.oneOf( [
+        'default',
+        'alert',
+        'info',
+        'error',
+        'success',
+    ] )
 };
 
 MessageBox.defaultProps =
 {
-    messageType : 'info',
+    messageType : 'default',
     cssMap      : require( './messageBox.css' )
 };
 
