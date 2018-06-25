276f064566b011170d3cb177011e4e74c989f197
daniel-martic-sociomantic <daniel.martic@sociomantic.com>
2018-06-25 15:26:55 +0200

    Updated Radio tests #397

--- src/Radio/tests.jsx
+++ src/Radio/tests.jsx
@@ -15,82 +15,84 @@ describe( 'Radio', () =>
 {
     let wrapper;
 
-    beforeEach(() =>
+    beforeEach( () =>
     {
         wrapper  = shallow( <Radio /> );
-    });
+    } );
 
     describe( 'render()', () =>
     {
-        test('should contain exactly one Checkable', () =>
+        test( 'should contain exactly one Checkable', () =>
         {
-            expect( wrapper.find( Checkable ) ).toHaveLength(1);
-        });
+            expect( wrapper.find( Checkable ) ).toHaveLength( 1 );
+        } );
     } );
 
     describe( 'props', () =>
     {
         describe( 'isDisabled', () =>
         {
-            test('should be passed to the Checkable', () =>
+            test( 'should be passed to the Checkable', () =>
             {
                 wrapper.setProps( { isDisabled: true } );
 
-                expect( wrapper.find( Checkable ).prop( 'isDisabled' ) ).toBe(true);
-            });
+                expect( wrapper.find( Checkable ).prop( 'isDisabled' ) )
+                    .toBe( true );
+            } );
         } );
 
         describe( 'hasError', () =>
         {
-            test('should be passed to the Checkable', () =>
+            test( 'should be passed to the Checkable', () =>
             {
                 wrapper.setProps( { hasError: true } );
 
-                expect( wrapper.find( Checkable ).prop( 'hasError' ) ).toBe(true);
-            });
+                expect( wrapper.find( Checkable ).prop( 'hasError' ) )
+                    .toBe( true );
+            } );
         } );
 
         describe( 'forceHover', () =>
         {
-            test('should be passed to the Checkable', () =>
+            test( 'should be passed to the Checkable', () =>
             {
                 wrapper.setProps( { forceHover: true } );
 
-                expect( wrapper.find( Checkable ).prop( 'forceHover' ) ).toBe(true);
-            });
+                expect( wrapper.find( Checkable ).prop( 'forceHover' ) )
+                    .toBe( true );
+            } );
         } );
 
         describe( 'onChange', () =>
         {
-            test('should be `undefined` if readOnly', () =>
+            test( 'should be `undefined` if readOnly', () =>
             {
-                const onChange = sinon.stub().callsFake( e =>
-                    targetChecked = e.target.checked
-                );
+                const onChange = jest.fn().mockImplementation( e =>
+                    targetChecked = e.target.checked );
                 wrapper.setProps( { isReadOnly: true, onChange } );
 
-                expect( wrapper.prop( onChange ) ).toBe(undefined);
-            });
+                expect( wrapper.prop( onChange ) ).toBeUndefined();
+            } );
 
-            test('should be passed as `undefined` to Checkable if readOnly', () =>
+            test( 'should be passed as `undefined` to Checkable if readOnly', () =>
             {
-                const onChange = sinon.stub().callsFake( e =>
-                    targetChecked = e.target.checked
-                );
+                const onChange = jest.fn().mockImplementation( e =>
+                    targetChecked = e.target.checked );
                 wrapper.setProps( { isReadOnly: true, onChange } );
 
-                expect( wrapper.find( Checkable ).prop( onChange ) ).toBe(undefined);
-            });
+                expect( wrapper.find( Checkable ).prop( onChange ) )
+                    .toBeUndefined();
+            } );
 
-            test('should be defined if not readOnly', () =>
+            test( 'should be defined if not readOnly', () =>
             {
-                const onChange = sinon.stub().callsFake( e =>
-                    targetChecked = e.target.checked
-                );
+                const onChange = jest.fn().mockImplementation( e =>
+                    targetChecked = e.target.checked );
                 wrapper.setProps( { onChange } );
 
-                expect( wrapper.prop( onChange ) ).toBe(wrapper.instance().onChange);
-            });
+                expect( wrapper.prop( onChange ) )
+                    .toBe( wrapper.instance().onChange );
+            } );
         } );
     } );
 } );
