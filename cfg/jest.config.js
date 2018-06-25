cd3d6a459f4c558ecfc5063b4bc0a135536ffa51
daniel-martic-sociomantic <daniel.martic@sociomantic.com>
2018-06-25 17:01:52 +0200

    Updated Spinner tests #397

--- cfg/jest.config.js
+++ cfg/jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
 
     testMatch :
     [
-        '<rootDir>/src/Sorter/tests.js?(x)'
+        '<rootDir>/src/Spinner/tests.js?(x)'
     ],
 
     // We should specify that jest should load all dependencies
--- src/Spinner/tests.jsx
+++ src/Spinner/tests.jsx
@@ -3,29 +3,27 @@
 /* eslint no-console: 0*/
 /* eslint-disable no-magic-numbers */
 
-// Uncomment the following lines to use the react test utilities
-import React                      from 'react';
-// const TestUtils = React.addons.TestUtils;
-import { mount }                  from 'enzyme';
+import React        from 'react';
+import { mount }    from 'enzyme';
 
-
-import Spinner                    from './index';
+import Spinner      from './index';
 
 describe( 'Spinner', () =>
 {
-    let Wrapper;
+    let wrapper;
 
-    beforeEach(() =>
-{
-        Wrapper = mount( <Spinner /> );
-    });
+    beforeEach( () =>
+    {
+        wrapper = mount( <Spinner /> );
+    } );
 
-    test('should have its component name as default className', () =>
-{
-        expect( Wrapper.find( '.spinner__default' ) ).toHaveLength(1);
-    });
-    test('should contain a Spinner', () =>
-{
-        expect( Wrapper.find( Spinner ) ).toHaveLength(1);
-    });
+    test( 'should have its component name as default className', () =>
+    {
+        expect( wrapper.find( `.${wrapper.prop( 'cssMap' ).default}` ) )
+            .toHaveLength( 1 );
+    } );
+    test( 'should contain a Spinner', () =>
+    {
+        expect( wrapper.find( Spinner ) ).toHaveLength( 1 );
+    } );
 } );
