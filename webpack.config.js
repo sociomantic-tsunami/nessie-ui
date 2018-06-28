09839c0a52e6dd6238f64dd883c23843fabe889e
daniel-martic-sociomantic <daniel.martic@sociomantic.com>
2018-06-28 14:22:21 +0200

    Removed webpack test in favor of jest #397

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,10 @@ const path = require( 'path' );
 const args = require( 'minimist' )( process.argv.slice( 2 ) );
 
 // List of allowed environments
-const allowedEnvs = [ 'dev', 'dist', 'test' ];
+const allowedEnvs = [ 'dev', 'dist' ];
 
 // Set the correct environment
 const env = allowedEnvs.includes( args.env ) ? args.env : 'dev';
 process.env.REACT_WEBPACK_ENV = env;
 
-module.exports = require( path.join( __dirname, `cfg/test` ) );
+module.exports = require( path.join( __dirname, `cfg/${env}` ) );
