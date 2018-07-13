a0a5c3a98f5e5f2b5c6d2090d6c2453e1335cdd3
Damian Rodriguez <damian.rodriguez@sociomantic.com>
2018-07-13 15:24:36 +0200

    Improve text styling options #436

--- src/Text/index.jsx
+++ src/Text/index.jsx
@@ -5,21 +5,30 @@ import { buildClassName }   from '../utils';
 import styles               from './text.css';
 
 const Text = ( {
+    allCaps,
     cssMap,
     className,
     children,
     noWrap,
     overflowIsHidden,
     role,
+    size,
+    spacing,
+    style,
     text,
-    textRef,
+    textColor,
+    textRef
 } ) => (
     <div
         className = { buildClassName( className, cssMap, {
+            allCaps,
             overflowHidden : overflowIsHidden,
             noWrap,
             role,
+            size,
+            style
         } ) }
+        style = { { letterSpacing: spacing, color: `#${textColor}` } }
         ref = { textRef }>
         { children || text }
     </div>
@@ -27,6 +36,10 @@ const Text = ( {
 
 Text.propTypes =
 {
+    /**
+     *  Capitalize text
+     */
+    allCaps          : PropTypes.bool,
     /**
     *  Don’t wrap text to the next line
     */
@@ -45,21 +58,57 @@ Text.propTypes =
         'critical'
     ] ),
     /**
+    *  Size to apply to text
+    */
+    size : PropTypes.oneOf( [
+        'XXXL',
+        'XXL',
+        'XL',
+        'L',
+        'M',
+        'S',
+        'XS',
+        'XXS'
+    ] ),
+    /**
+    *  Style to apply to text
+    */
+    style : PropTypes.oneOf( [
+        'Light',
+        'Regular',
+        'RegularIt',
+        'SemiBold',
+        'Bold',
+        'ExtraBold'
+    ] ),
+    /**
+    * Letter Spacing for the text
+    */
+    spacing   : PropTypes.string,
+    /**
     *  Text string
     */
-    text    : PropTypes.string,
+    text      : PropTypes.string,
+    /**
+    *  Text Color
+    */
+    textColor : PropTypes.string,
     /**
     *  callback that receives ref to the text div: ref => ...
     */
-    textRef : PropTypes.func,
+    textRef   : PropTypes.func
 };
 
 Text.defaultProps =
 {
+    allCaps          : false,
     cssMap           : styles,
     noWrap           : false,
     overflowIsHidden : false,
     role             : 'default',
+    size             : 'M',
+    spacing          : '0',
+    style            : 'Regular'
 };
 
 export default Text;
