c070ae111aef4f841ac3599e60373dea5416f0cc
Josh Wilson <joshuef@gmail.com>
2018-07-17 11:05:23 +0200

    fix/IconButton: onMouseDown tweak to avoid react warning

--- src/IconButton/index.jsx
+++ src/IconButton/index.jsx
@@ -193,7 +193,7 @@ export default class IconButton extends Component
                     onBlur    = { onBlur }
                     onFocus   = { onFocus }
                     tabIndex  = { isFocusable ? '0' : '-1' }
-                    onMouseDown = { !isFocusable && killFocus }>
+                    onMouseDown = { !isFocusable ? killFocus : undefined }>
                     <Icon
                         className  = { cssMap.icon }
                         size       = { iconSize }
