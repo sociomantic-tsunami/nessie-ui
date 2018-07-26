b30d44a404918e3c1fc8b0bfacc0da5b40b41689
Conor Cafferkey <conor.cafferkey@sociomantic.com>
2018-07-26 21:32:05 +0200

    Converted React.PureComponent’s to functions #425

--- src/Tooltip/index.jsx
+++ src/Tooltip/index.jsx
@@ -1,171 +1,188 @@
-import React                            from 'react';
-import PropTypes                        from 'prop-types';
+import React                          from 'react';
+import PropTypes                      from 'prop-types';
 
-import { generateId, buildClassName }   from '../utils';
-import IconButton                       from '../IconButton';
-import Text                             from '../Text';
+import { generateId, buildClassName } from '../utils';
+import IconButton                     from '../IconButton';
+import Text                           from '../Text';
+import styles                         from './tooltip.css';
 
-export default class Tooltip extends React.PureComponent
+const Tooltip = ( {
+    children,
+    className,
+    cssMap,
+    id = generateId( 'Tooltip' ),
+    isDismissible,
+    isVisible,
+    message,
+    noWrap,
+    onClickClose,
+    onMouseOut,
+    onMouseOver,
+    overflowIsHidden,
+    position,
+    role,
+} ) =>
 {
-    static propTypes =
+    let messageText = message;
+
+    if ( typeof messageText === 'string' )
     {
-        /**
-         *  Node that the Tooltip wraps
-         */
-        children         : PropTypes.node,
-        /**
-         * HTML id attribute (overwrite default)
-         */
-        id               : PropTypes.string,
-        /**
-         *  Display the tooltip as user dismissible
-         */
-        isDismissible    : PropTypes.bool,
-        /**
-         *  Display the tooltip
-         */
-        isVisible        : PropTypes.bool,
-        /**
-         *  Tooltip message text (string or JSX)
-         */
-        message          : PropTypes.node,
-        /**
-         *  Text won’t wrap to the next line
-         */
-        noWrap           : PropTypes.bool,
-        /**
-         *  Function to call on “Close” button click: ( e ) => { ... }
-         */
-        onClickClose     : PropTypes.func,
-        /**
-         *  onMouseOver callback function: ( e ) => { ... }
-         */
-        onMouseOver      : PropTypes.func,
-        /**
-         *  onMouseOut callback function: ( e ) => { ... }
-         */
-        onMouseOut       : PropTypes.func,
-        /**
-         *  Hides overflow of wrapped content
-         */
-        overflowIsHidden : PropTypes.bool,
-        /**
-         *  Tooltip position relative to wrapped component
-         */
-        position         : PropTypes.oneOf( [
-            'left',
-            'right',
-            'top',
-            'bottom',
-            'topLeft',
-            'topRight',
-            'bottomLeft',
-            'bottomRight' ] ),
-        /**
-         *  Tooltip role/style
-         */
-        role : PropTypes.oneOf( [
-            'default',
-            'critical',
-            'promoted',
-            'warning'
-        ] )
-    };
+        const regexp = /([^\s].{1,49}(?=\s+|$))|([^\s]{1,50})/g;
 
-    static defaultProps =
-    {
-        position       : 'top',
-        id             : undefined,
-        isVisible      : true,
-        noWrap         : false,
-        overflowHidden : false,
-        cssMap         : require( './tooltip.css' ),
-        role           : 'default'
-    };
+        const matches = message.match( regexp ) || [];
 
-    render()
-    {
-        const {
-            children,
-            className,
-            cssMap,
-            message,
-            position,
-            id = generateId( 'Tooltip' ),
-            isDismissible,
-            isVisible,
-            noWrap,
-            onClickClose,
-            onMouseOver,
-            onMouseOut,
-            overflowIsHidden,
-            role
-        } = this.props;
-
-        let messageText = message;
-
-        if ( typeof messageText === 'string' )
-        {
-            const regexp = /([^\s].{1,49}(?=\s+|$))|([^\s]{1,50})/g;
-
-            const matches = message.match( regexp ) || [];
-
-            messageText = (
-                <Text className = { cssMap.messageText } noWrap>
-                    { matches.map( line => [ line, <br /> ] ) }
-                </Text> );
-        }
+        messageText = (
+            <Text className = { cssMap.messageText } noWrap>
+                { matches.map( line => [ line, <br /> ] ) }
+            </Text> );
+    }
 
 
-        const tooltip = (
-            <div
-                className = { cssMap.tooltip }
-                role      = "tooltip"
-                id        = { id }>
-                <div className = { cssMap.message }>
-                    { messageText }
-                </div>
-                { isDismissible &&
-                    <div className = { cssMap.iconContainer } >
-                        <IconButton
-                            iconType  = "close"
-                            onClick   = { onClickClose }
-                            iconTheme = "button"
-                            iconSize  = "M" />
-                    </div>
-                }
+    const tooltip = (
+        <div
+            className = { cssMap.tooltip }
+            role      = "tooltip"
+            id        = { id }>
+            <div className = { cssMap.message }>
+                { messageText }
             </div>
+            { isDismissible &&
+                <div className = { cssMap.iconContainer } >
+                    <IconButton
+                        iconType  = "close"
+                        onClick   = { onClickClose }
+                        iconTheme = "button"
+                        iconSize  = "M" />
+                </div>
+            }
+        </div>
+    );
+
+    let contentNode = children;
+
+    if ( typeof children === 'string' )
+    {
+        contentNode = (
+            <Text
+                overflowIsHidden = { overflowIsHidden }
+                noWrap           = { noWrap }>
+                { children }
+            </Text>
         );
+    }
 
-        let contentNode = children;
+    return (
 
-        if ( typeof children === 'string' )
-        {
-            contentNode = (
-                <Text
-                    overflowIsHidden = { overflowIsHidden }
-                    noWrap           = { noWrap }>
-                    { children }
-                </Text>
-            );
-        }
-
-        return (
-
-            <div
-                className    = { buildClassName( className, cssMap, { role, noWrap, position } ) }
-                onMouseEnter = { onMouseOver }
-                onMouseLeave = { onMouseOut }>
-                { contentNode &&
+        <div
+            className = { buildClassName( className, cssMap, {
+                role,
+                noWrap,
+                position,
+            } ) }
+            onMouseEnter = { onMouseOver }
+            onMouseLeave = { onMouseOut }>
+            { contentNode &&
                 <div
                     className        = { cssMap.content }
                     aria-describedby = { isVisible ? id : null }>
                     { contentNode }
                 </div>
-                }
-                { isVisible && tooltip }
-            </div>
+            }
+            { isVisible && tooltip }
+        </div>
 
-        );
-    }
-}
+    );
+};
+
+Tooltip.propTypes =
+{
+    /**
+     *  Node that the Tooltip wraps
+     */
+    children         : PropTypes.node,
+    /**
+     * Extra CSS class name
+     */
+    className        : PropTypes.string,
+    /**
+     *  CSS class map
+     */
+    cssMap           : PropTypes.objectOf( PropTypes.string ),
+    /**
+     * HTML id attribute (overwrite default)
+     */
+    id               : PropTypes.string,
+    /**
+     *  Display the tooltip as user dismissible
+     */
+    isDismissible    : PropTypes.bool,
+    /**
+     *  Display the tooltip
+     */
+    isVisible        : PropTypes.bool,
+    /**
+     *  Tooltip message text (string or JSX)
+     */
+    message          : PropTypes.node,
+    /**
+     *  Text won’t wrap to the next line
+     */
+    noWrap           : PropTypes.bool,
+    /**
+     *  Function to call on “Close” button click: ( e ) => { ... }
+     */
+    onClickClose     : PropTypes.func,
+    /**
+     *  onMouseOver callback function: ( e ) => { ... }
+     */
+    onMouseOver      : PropTypes.func,
+    /**
+     *  onMouseOut callback function: ( e ) => { ... }
+     */
+    onMouseOut       : PropTypes.func,
+    /**
+     *  Hides overflow of wrapped content
+     */
+    overflowIsHidden : PropTypes.bool,
+    /**
+     *  Tooltip position relative to wrapped component
+     */
+    position         : PropTypes.oneOf( [
+        'left',
+        'right',
+        'top',
+        'bottom',
+        'topLeft',
+        'topRight',
+        'bottomLeft',
+        'bottomRight' ] ),
+    /**
+     *  Tooltip role/style
+     */
+    role : PropTypes.oneOf( [
+        'default',
+        'critical',
+        'promoted',
+        'warning',
+    ] ),
+};
+
+Tooltip.defaultProps =
+{
+    children         : undefined,
+    className        : undefined,
+    cssMap           : styles,
+    id               : undefined,
+    isDismissible    : undefined,
+    isVisible        : true,
+    message          : undefined,
+    noWrap           : false,
+    onClickClose     : undefined,
+    onMouseOut       : undefined,
+    onMouseOver      : undefined,
+    overflowIsHidden : false,
+    position         : 'top',
+    role             : 'default',
+};
+
+export default Tooltip;
--- src/Uploader/index.jsx
+++ src/Uploader/index.jsx
@@ -1,257 +1,273 @@
-import React                            from 'react';
-import PropTypes                        from 'prop-types';
+import React                          from 'react';
+import PropTypes                      from 'prop-types';
+
+import { generateId, buildClassName } from '../utils';
+import Button                         from '../Button';
+import IconWithTooltip                from '../IconWithTooltip';
+import Spinner                        from '../Spinner';
+import IconButton                     from '../IconButton';
+import Tooltip                        from '../Tooltip';
+import Label                          from '../Label';
+import styles                         from './uploader.css';
 
 
-import { generateId, buildClassName }   from '../utils';
-import Button                           from '../Button';
-import IconWithTooltip                  from '../IconWithTooltip';
-import Spinner                          from '../Spinner';
-import IconButton                       from '../IconButton';
-import Tooltip                          from '../Tooltip';
-import Label                            from '../Label';
-
-export default class Uploader extends React.PureComponent
+const Uploader = ( {
+    buttonLabel,
+    className,
+    cssMap,
+    errorMessage,
+    errorMessagePosition,
+    hasError,
+    hasWarning,
+    id = generateId( 'Uploader' ),
+    isDisabled,
+    isReadOnly,
+    label,
+    onChange,
+    onClick,
+    onClickSecondary,
+    onMouseOut,
+    onMouseOver,
+    previewIsDisabled,
+    previewTooltipIsVisible,
+    previewTooltipMessage,
+    tooltipIsVisible,
+    uploadState,
+    warningMessage,
+} ) =>
 {
-    static propTypes =
+    const buttonRole = 'default';
+    let hasTooltip = false;
+    let uploaded   = false;
+    let isLoading  = false;
+    let iconType   = 'upload';
+    const iconTheme  = 'button';
+
+    let uploaderButtonClass = cssMap.uploadButton;
+    let messageType;
+    let message;
+
+
+    if ( uploadState === 'uploading' )
     {
-        /**
-        *  Label text
-        */
-        label       : PropTypes.string,
-        /**
-        *  “Upload” Button text
-        */
-        buttonLabel : PropTypes.string,
-        /**
-        *  Uploader state
-        */
-        uploadState : PropTypes.oneOf( [ 'default',
-            'uploading',
-            'uploaded' ] ),
-        /**
-        *  Display as disabled
-        */
-        isDisabled              : PropTypes.bool,
-        /**
-         * HTML id attribute (overwrite default)
-         */
-        id                      : PropTypes.string,
-        /**
-        *  Display as read-only
-        */
-        isReadOnly              : PropTypes.bool,
-        /**
-        *  Display as error/invalid
-        */
-        hasError                : PropTypes.bool,
-        /**
-        *  Tooltip error message text (string or JSX)
-        */
-        errorMessage            : PropTypes.node,
-        /**
-        *  Display as warning ( adds warning icon )
-        */
-        hasWarning              : PropTypes.bool,
-        /**
-        *  Tooltip warning message text (string or JSX)
-        */
-        warningMessage          : PropTypes.node,
-        /**
-         * Error or warning tooltip is visible
-         */
-        tooltipIsVisible        : PropTypes.bool,
-        /**
-         * Preview tootltip messsage text (string or JSX)
-         */
-        previewTooltipMessage   : PropTypes.node,
-        /**
-         * Preview tootltip is visible
-         */
-        previewTooltipIsVisible : PropTypes.bool,
-        /**
-         * Preview button is disabled
-         */
-        previewIsDisabled       : PropTypes.bool,
-        /**
-         * onClick callback function: ( e ) => { ... }
-         */
-        onClick                 : PropTypes.func,
-        /**
-         * onClickSecondary callback function: ( e ) => { ... }
-         */
-        onClickSecondary        : PropTypes.func,
-        /**
-         * onChange callback function: ( e ) => { ... }
-         */
-        onChange                : PropTypes.func,
-        /**
-         * onMouseOut callback function: ( e ) => { ... }
-         */
-        onMouseOut              : PropTypes.func,
-        /**
-         * onMouseOver callback function: ( e ) => { ... }
-         */
-        onMouseOver             : PropTypes.func,
-        /**
-        *  Error message position relative to the icon
-        */
-        errorMessagePosition    : PropTypes.oneOf( [ 'left',
-            'right',
-            'top',
-            'bottom',
-            'topLeft',
-            'topRight' ] )
-    };
-
-    static defaultProps =
+        isLoading  = true;
+        iconType   = 'none';
+        uploaderButtonClass = cssMap.uploaderButton;
+    }
+    else if ( uploadState === 'uploaded' )
     {
-        uploadState             : 'default',
-        hasError                : false,
-        hasWarning              : false,
-        tooltipIsVisible        : false,
-        errorMessagePosition    : 'top',
-        id                      : undefined,
-        isDisabled              : false,
-        isReadOnly              : false,
-        previewTooltipIsVisible : false,
-        buttonLabel             : 'Upload',
-        cssMap                  : require( './uploader.css' )
-    };
+        uploaded   = true;
+        iconType   = 'none';
+        isLoading  = false;
+        uploaderButtonClass = cssMap.uploaderButton;
+    }
 
-    render()
+
+    if ( hasWarning )
     {
-        const {
-            buttonLabel,
-            className,
-            cssMap,
-            errorMessage,
-            hasError,
-            hasWarning,
-            id = generateId( 'Uploader' ),
-            isDisabled,
-            isReadOnly,
-            label,
-            onChange,
-            onClick,
-            onClickSecondary,
-            onMouseOut,
-            onMouseOver,
-            previewIsDisabled,
-            previewTooltipIsVisible,
-            previewTooltipMessage,
-            tooltipIsVisible,
-            errorMessagePosition,
-            uploadState,
-            warningMessage
-        } = this.props;
-
-        const buttonRole = 'default';
-        let hasTooltip = false;
-        let uploaded   = false;
-        let isLoading  = false;
-        let iconType   = 'upload';
-        const iconTheme  = 'button';
-
-        let uploaderButtonClass = cssMap.uploadButton;
-        let messageType;
-        let message;
+        hasTooltip  = true;
+        messageType = 'alert';
+        message     = warningMessage;
+    }
+    if ( hasError )
+    {
+        hasTooltip  = true;
+        messageType = 'error';
+        message     = errorMessage;
+    }
 
 
-        if ( uploadState === 'uploading' )
-        {
-            isLoading  = true;
-            iconType   = 'none';
-            uploaderButtonClass = cssMap.uploaderButton;
-        }
-        else if ( uploadState === 'uploaded' )
-        {
-            uploaded   = true;
-            iconType   = 'none';
-            isLoading  = false;
-            uploaderButtonClass = cssMap.uploaderButton;
-        }
+    return (
 
-
-        if ( hasWarning )
-        {
-            hasTooltip  = true;
-            messageType = 'alert';
-            message     = warningMessage;
-        }
-        if ( hasError )
-        {
-            hasTooltip  = true;
-            messageType = 'error';
-            message     = errorMessage;
-        }
-
-
-        return (
-
-            <div
-                className    = { buildClassName( className, cssMap, {
-                    loading         : isLoading,
-                    uploaded,
-                    disabled        : isDisabled,
-                    previewDisabled : previewIsDisabled
-                } ) }
-                onMouseEnter = { onMouseOver }
-                onMouseLeave = { onMouseOut } >
-                <input
-                    type         = "file"
-                    name         = { `${id}-file` }
-                    className    = { cssMap.input }
-                    onChange     = { onChange } />
-                { label &&
-                <Label
-                    overflowIsHidden
-                    htmlFor    = { `${id}-file` }
-                    isDisabled = { isDisabled }>
-                    { label }
-                </Label>
-                }
-                <IconWithTooltip
-                    className        = { cssMap.iconWithTooltip }
-                    iconType         = { messageType }
-                    iconPosition     = "topRight"
-                    message          = { message }
-                    iconIsVisible    = { hasTooltip }
-                    tooltipPosition  = { errorMessagePosition }
-                    tooltipIsVisible = { tooltipIsVisible }>
-                    <div className = { cssMap.buttonsContainer }>
-                        <Tooltip
-                            isVisible = { uploadState ===
-                                'uploaded' && previewTooltipIsVisible }
-                            message   = { previewTooltipMessage }
-                            className = { cssMap.previewTooltip }>
-                            <Button
-                                role       = { buttonRole }
-                                className  = { uploaderButtonClass }
-                                onClick    = { onClick }
-                                isDisabled = { isDisabled }
-                                isReadOnly = { isReadOnly }
-                                iconType   = { iconType }>
-                                { buttonLabel }
-                            </Button>
-                        </Tooltip>
-                        { isLoading &&
-                        <div className = { cssMap.loadingOverlay }>
-                            <Spinner className = { cssMap.spinner } />
-                        </div>
-                        }
-                        { uploaded  &&
-                        <IconButton
-                            className  = { cssMap.uploadedButton }
-                            onClick    = { onClickSecondary }
+        <div
+            className    = { buildClassName( className, cssMap, {
+                loading         : isLoading,
+                uploaded,
+                disabled        : isDisabled,
+                previewDisabled : previewIsDisabled,
+            } ) }
+            onMouseEnter = { onMouseOver }
+            onMouseLeave = { onMouseOut } >
+            <input
+                type         = "file"
+                name         = { `${id}-file` }
+                className    = { cssMap.input }
+                onChange     = { onChange } />
+            { label &&
+            <Label
+                overflowIsHidden
+                htmlFor    = { `${id}-file` }
+                isDisabled = { isDisabled }>
+                { label }
+            </Label>
+            }
+            <IconWithTooltip
+                className        = { cssMap.iconWithTooltip }
+                iconType         = { messageType }
+                iconPosition     = "topRight"
+                message          = { message }
+                iconIsVisible    = { hasTooltip }
+                tooltipPosition  = { errorMessagePosition }
+                tooltipIsVisible = { tooltipIsVisible }>
+                <div className = { cssMap.buttonsContainer }>
+                    <Tooltip
+                        isVisible = { uploadState ===
+                            'uploaded' && previewTooltipIsVisible }
+                        message   = { previewTooltipMessage }
+                        className = { cssMap.previewTooltip }>
+                        <Button
+                            role       = { buttonRole }
+                            className  = { uploaderButtonClass }
+                            onClick    = { onClick }
                             isDisabled = { isDisabled }
                             isReadOnly = { isReadOnly }
-                            iconType   = "delete"
-                            iconTheme  = { iconTheme } />
-                        }
+                            iconType   = { iconType }>
+                            { buttonLabel }
+                        </Button>
+                    </Tooltip>
+                    { isLoading &&
+                    <div className = { cssMap.loadingOverlay }>
+                        <Spinner className = { cssMap.spinner } />
                     </div>
-                </IconWithTooltip>
-            </div>
+                    }
+                    { uploaded  &&
+                    <IconButton
+                        className  = { cssMap.uploadedButton }
+                        onClick    = { onClickSecondary }
+                        isDisabled = { isDisabled }
+                        isReadOnly = { isReadOnly }
+                        iconType   = "delete"
+                        iconTheme  = { iconTheme } />
+                    }
+                </div>
+            </IconWithTooltip>
+        </div>
 
-        );
-    }
-}
+    );
+};
+
+Uploader.propTypes =
+{
+    /**
+     *  CSS class map
+     */
+    cssMap      : PropTypes.objectOf( PropTypes.string ),
+    /**
+    *  Label text
+    */
+    label       : PropTypes.string,
+    /**
+    *  “Upload” Button text
+    */
+    buttonLabel : PropTypes.string,
+    /**
+    *  Uploader state
+    */
+    uploadState : PropTypes.oneOf( [
+        'default',
+        'uploading',
+        'uploaded',
+    ] ),
+    /**
+    *  Display as disabled
+    */
+    isDisabled              : PropTypes.bool,
+    /**
+     * HTML id attribute (overwrite default)
+     */
+    id                      : PropTypes.string,
+    /**
+    *  Display as read-only
+    */
+    isReadOnly              : PropTypes.bool,
+    /**
+    *  Display as error/invalid
+    */
+    hasError                : PropTypes.bool,
+    /**
+    *  Tooltip error message text (string or JSX)
+    */
+    errorMessage            : PropTypes.node,
+    /**
+    *  Display as warning ( adds warning icon )
+    */
+    hasWarning              : PropTypes.bool,
+    /**
+    *  Tooltip warning message text (string or JSX)
+    */
+    warningMessage          : PropTypes.node,
+    /**
+     * Error or warning tooltip is visible
+     */
+    tooltipIsVisible        : PropTypes.bool,
+    /**
+     * Preview tootltip messsage text (string or JSX)
+     */
+    previewTooltipMessage   : PropTypes.node,
+    /**
+     * Preview tootltip is visible
+     */
+    previewTooltipIsVisible : PropTypes.bool,
+    /**
+     * Preview button is disabled
+     */
+    previewIsDisabled       : PropTypes.bool,
+    /**
+     * onClick callback function: ( e ) => { ... }
+     */
+    onClick                 : PropTypes.func,
+    /**
+     * onClickSecondary callback function: ( e ) => { ... }
+     */
+    onClickSecondary        : PropTypes.func,
+    /**
+     * onChange callback function: ( e ) => { ... }
+     */
+    onChange                : PropTypes.func,
+    /**
+     * onMouseOut callback function: ( e ) => { ... }
+     */
+    onMouseOut              : PropTypes.func,
+    /**
+     * onMouseOver callback function: ( e ) => { ... }
+     */
+    onMouseOver             : PropTypes.func,
+    /**
+    *  Error message position relative to the icon
+    */
+    errorMessagePosition    : PropTypes.oneOf( [
+        'left',
+        'right',
+        'top',
+        'bottom',
+        'topLeft',
+        'topRight',
+    ] ),
+};
+
+Uploader.defaultProps =
+{
+    buttonLabel             : 'Upload',
+    cssMap                  : styles,
+    errorMessage            : undefined,
+    errorMessagePosition    : 'top',
+    hasError                : false,
+    hasWarning              : false,
+    id                      : undefined,
+    isDisabled              : false,
+    isReadOnly              : false,
+    label                   : undefined,
+    onChange                : undefined,
+    onClick                 : undefined,
+    onClickSecondary        : undefined,
+    onMouseOut              : undefined,
+    onMouseOver             : undefined,
+    previewIsDisabled       : undefined,
+    previewTooltipIsVisible : false,
+    previewTooltipMessage   : undefined,
+    tooltipIsVisible        : false,
+    uploadState             : 'default',
+    warningMessage          : undefined,
+};
+
+export default Uploader;
