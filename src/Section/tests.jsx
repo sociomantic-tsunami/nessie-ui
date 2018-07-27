2a3dc95c17702ec239a95c12c2f13c96f6731269
Conor Cafferkey <conor.cafferkey@sociomantic.com>
2018-07-27 09:38:34 +0200

    Updated tests for function components #425

--- src/Section/tests.jsx
+++ src/Section/tests.jsx
@@ -4,10 +4,10 @@
 import React              from 'react';
 import { mount, shallow } from 'enzyme';
 
-
 import { H1, H4 }         from '../index';
 
 import Section            from './index';
+
 
 describe( 'Section', () =>
 {
@@ -20,29 +20,19 @@ describe( 'Section', () =>
         instance = wrapper.instance();
     } );
 
-    describe( 'constructor( props )', () =>
+    test( 'should be an instance of StatelessComponent', () =>
     {
-        test( 'should have name Button', () =>
-        {
-            expect( instance.constructor.name ).toBe( 'Section' );
-        } );
+        expect( instance.constructor.name ).toBe( 'StatelessComponent' );
     } );
 
-    describe( 'render()', () =>
+    test( 'should have a header component corresponding to level prop', () =>
     {
-      
-        test(
-            'should have a header component corresponding to level prop',
-            () =>
-            {
-                wrapper.setProps( {
-                    title : 'Boom',
-                    level : 4
-                } );
+        wrapper.setProps( {
+            title : 'Boom',
+            level : 4,
+        } );
 
-                expect( wrapper.find( H4 ) ).toHaveLength( 1 );
-            }
-        );
+        expect( wrapper.find( H4 ) ).toHaveLength( 1 );
     } );
 
     describe( 'props', () =>
@@ -58,7 +48,7 @@ describe( 'Section', () =>
             {
                 wrapper.setProps( {
                     title : 'Boom',
-                    level : 1
+                    level : 1,
                 } );
 
                 expect( wrapper.find( H1 ).prop( 'children' ) ).toBe( 'Boom' );
