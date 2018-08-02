6a2498cb6df6fddaddf79985d1bfb1f344b10263
Daniel <daniel.martic@sociomantic.com>
2018-08-02 15:14:13 +0200

    Updated NotificationBar tests #267

--- src/NotificationBar/tests.jsx
+++ src/NotificationBar/tests.jsx
@@ -1,4 +1,4 @@
-/* global test */
+/* global jest test */
 /* eslint no-console: 0*/
 /* eslint-disable no-magic-numbers, no-multi-str, no-unused-expressions */
 
@@ -21,5 +21,33 @@ describe( 'NotificationBar', () =>
     {
         expect( wrapper.find( `.${wrapper.prop( 'cssMap' ).default}` ).first() )
             .toHaveLength( 1 );
+    } );
+} );
+
+
+describe( 'NotificationBarDriver', () =>
+{
+    let wrapper;
+    let driver;
+
+    beforeEach( () =>
+    {
+        wrapper = mount( <NotificationBar /> );
+        driver = wrapper.driver();
+    } );
+
+
+    describe( 'clickClose()', () =>
+    {
+        test( 'should trigger onClickClose callback once', () =>
+        {
+            const onClickClose = jest.fn();
+
+            wrapper.setProps( { onClickClose, isDismissible: true } );
+
+            driver.clickClose();
+
+            expect( onClickClose ).toBeCalledTimes( 1 );
+        } );
     } );
 } );
