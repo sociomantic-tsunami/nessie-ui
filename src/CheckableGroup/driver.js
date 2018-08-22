ac7cc9912856d5438a46f1c9b5a1fc416936877d
Daniel <daniel.martic@sociomantic.com>
2018-08-22 13:12:32 +0200

    Updated CheckableGroup errors #516

--- src/CheckableGroup/driver.js
+++ src/CheckableGroup/driver.js
@@ -1,13 +1,8 @@
 import { Checkbox, Radio } from '../index';
 
-const ERRORS = {
-    CHECKABLEGROUP_CANNOT_BE_CHANGED : ( label, state ) =>
-        `CheckableGroup '${label}' cannot be changed since it is ${state}`,
-    CHECKABLEGROUP_CANNOT_MOUSEOVER : ( label, state ) =>
-        `CheckableGroup '${label}' cannot have onMouseOver since it is \
-${state}`,
-    CHECKABLEGROUP_CANNOT_MOUSEOUT : ( label, state ) =>
-        `CheckableGroup '${label}' cannot have onMouseOut since it is ${state}`,
+const ERR = {
+    CHECKABLEGROUP_ERR : ( label, doWhat, state ) => `CheckableGroup \
+'${label}' can't trigger ${doWhat} since it is ${state}`,
 };
 
 export default class CheckableGroupDriver
@@ -29,14 +24,14 @@ export default class CheckableGroupDriver
 
         if ( props.isDisabled )
         {
-            throw new Error( ERRORS
-                .CHECKABLEGROUP_CANNOT_BE_CHANGED( label, 'disabled' ) );
+            throw new Error( ERR
+                .CHECKABLEGROUP_ERR( label, 'onChange', 'disabled' ) );
         }
 
         if ( props.isReadOnly )
         {
-            throw new Error( ERRORS
-                .CHECKABLEGROUP_CANNOT_BE_CHANGED( label, 'read only' ) );
+            throw new Error( ERR
+                .CHECKABLEGROUP_ERR( label, 'onChange', 'read only' ) );
         }
 
 
@@ -46,42 +41,12 @@ export default class CheckableGroupDriver
 
     mouseOver()
     {
-        const props     = this.wrapper.props();
-        const { label } = props;
-
-        if ( props.isDisabled )
-        {
-            throw new Error( ERRORS
-                .CHECKABLEGROUP_CANNOT_MOUSEOVER( label, 'disabled' ) );
-        }
-
-        if ( props.isReadOnly )
-        {
-            throw new Error( ERRORS
-                .CHECKABLEGROUP_CANNOT_MOUSEOVER( label, 'read only' ) );
-        }
-
         this.wrapper.simulate( 'mouseenter' );
         return this;
     }
 
     mouseOut()
     {
-        const props     = this.wrapper.props();
-        const { label } = props;
-
-        if ( props.isDisabled )
-        {
-            throw new Error( ERRORS
-                .CHECKABLEGROUP_CANNOT_MOUSEOUT( label, 'disabled' ) );
-        }
-
-        if ( props.isReadOnly )
-        {
-            throw new Error( ERRORS
-                .CHECKABLEGROUP_CANNOT_MOUSEOUT( label, 'read only' ) );
-        }
-
         this.wrapper.simulate( 'mouseleave' );
         return this;
     }
--- src/CheckableGroup/tests.jsx
+++ src/CheckableGroup/tests.jsx
@@ -45,8 +45,8 @@ describe( 'CheckableGroupDriver', () =>
             {
                 wrapper.setProps( { isDisabled: true, label: 'Tekeli-li' } );
 
-                const expectedError = 'CheckableGroup \'Tekeli-li\' cannot \
-be changed since it is disabled';
+                const expectedError = 'CheckableGroup \'Tekeli-li\' can\'t \
+trigger onChange since it is disabled';
 
                 expect( () => driver.change() ).toThrow( expectedError );
             } );
@@ -72,8 +72,8 @@ be changed since it is disabled';
             {
                 wrapper.setProps( { isReadOnly: true, label: 'Tekeli-li' } );
 
-                const expectedError = 'CheckableGroup \'Tekeli-li\' cannot \
-be changed since it is read only';
+                const expectedError = 'CheckableGroup \'Tekeli-li\' can\'t \
+trigger onChange since it is read only';
 
                 expect( () => driver.change() ).toThrow( expectedError );
             } );
@@ -105,60 +105,6 @@ be changed since it is read only';
 
             expect( onMouseOver ).toBeCalledTimes( 1 );
         } );
-
-
-        describe( 'isDisabled', () =>
-        {
-            test( 'throws the expected error when isDisabled', () =>
-            {
-                wrapper.setProps( { isDisabled: true, label: 'Tekeli-li' } );
-
-                const expectedError = 'CheckableGroup \'Tekeli-li\' cannot \
-have onMouseOver since it is disabled';
-
-                expect( () => driver.mouseOver() ).toThrow( expectedError );
-            } );
-
-            test( 'should not trigger onMouseOver when isDisabled', () =>
-            {
-                const onMouseOver = jest.fn();
-                wrapper.setProps( {
-                    isDisabled : true,
-                    label      : 'Tekeli-li',
-                    onMouseOver,
-                } );
-
-                expect( () => driver.mouseOver() );
-                expect( onMouseOver ).not.toBeCalled();
-            } );
-        } );
-
-
-        describe( 'isReadOnly', () =>
-        {
-            test( 'throws the expected error when isReadOnly', () =>
-            {
-                wrapper.setProps( { isReadOnly: true, label: 'Tekeli-li' } );
-
-                const expectedError = 'CheckableGroup \'Tekeli-li\' cannot \
-have onMouseOver since it is read only';
-
-                expect( () => driver.mouseOver() ).toThrow( expectedError );
-            } );
-
-            test( 'should not trigger onMouseOver when isReadOnly', () =>
-            {
-                const mouseOver = jest.fn();
-                wrapper.setProps( {
-                    isReadOnly : true,
-                    label      : 'Tekeli-li',
-                    mouseOver,
-                } );
-
-                expect( () => driver.mouseOver() );
-                expect( mouseOver ).not.toBeCalled();
-            } );
-        } );
     } );
 
 
@@ -172,60 +118,6 @@ have onMouseOver since it is read only';
             driver.mouseOut();
 
             expect( onMouseOut ).toBeCalledTimes( 1 );
-        } );
-
-
-        describe( 'isDisabled', () =>
-        {
-            test( 'throws the expected error when isDisabled', () =>
-            {
-                wrapper.setProps( { isDisabled: true, label: 'Tekeli-li' } );
-
-                const expectedError = 'CheckableGroup \'Tekeli-li\' cannot \
-have onMouseOut since it is disabled';
-
-                expect( () => driver.mouseOut() ).toThrow( expectedError );
-            } );
-
-            test( 'should not trigger onMouseOut when isDisabled', () =>
-            {
-                const onMouseOut = jest.fn();
-                wrapper.setProps( {
-                    isDisabled : true,
-                    label      : 'Tekeli-li',
-                    onMouseOut,
-                } );
-
-                expect( () => driver.mouseOut() );
-                expect( onMouseOut ).not.toBeCalled();
-            } );
-        } );
-
-
-        describe( 'isReadOnly', () =>
-        {
-            test( 'throws the expected error when isReadOnly', () =>
-            {
-                wrapper.setProps( { isReadOnly: true, label: 'Tekeli-li' } );
-
-                const expectedError = 'CheckableGroup \'Tekeli-li\' cannot \
-have onMouseOut since it is read only';
-
-                expect( () => driver.mouseOut() ).toThrow( expectedError );
-            } );
-
-            test( 'should not trigger onMouseOut when isReadOnly', () =>
-            {
-                const onMouseOut = jest.fn();
-                wrapper.setProps( {
-                    isReadOnly : true,
-                    label      : 'Tekeli-li',
-                    onMouseOut,
-                } );
-
-                expect( () => driver.mouseOut() );
-                expect( onMouseOut ).not.toBeCalled();
-            } );
         } );
     } );
 } );
