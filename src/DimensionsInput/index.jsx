bffa993369465c46c1bbce8e2f02f464c20c51a8
Daniel <daniel.martic@sociomantic.com>
2018-09-19 17:43:03 +0200

    Updated DimensionsInput #266

--- src/DimensionsInput/index.jsx
+++ src/DimensionsInput/index.jsx
@@ -13,148 +13,168 @@ import PropTypes                         from 'prop-types';
 import { generateId }                    from '../utils';
 import { Column, InputField, Row, Text } from '../index';
 
-const DimensionsInput = ( {
-    className,
-    forceHover,
-    hasError,
-    heightPlaceholder,
-    heightValue,
-    id = generateId( 'DimensionsInput' ),
-    isDisabled,
-    isReadOnly,
-    onBlur,
-    onChange,
-    onFocus,
-    onMouseOut,
-    onMouseOver,
-    widthPlaceholder,
-    widthValue,
-} ) => (
-    <Row
-        className     = { className }
-        gutters       = "S"
-        onMouseOut    = { onMouseOut }
-        onMouseOver   = { onMouseOver }
-        verticalAlign = "middle">
-        <Column>
-            <InputField
-                autoCapitalize = "off"
-                autoComplete   = "off"
-                autoCorrect    = "off"
-                forceHover     = { forceHover }
-                hasError       = { hasError }
-                id             = { `${id}-width` }
-                isDisabled     = { isDisabled }
-                isReadOnly     = { isReadOnly }
-                onBlur         = { onBlur }
-                onChange       = { onChange }
-                onFocus        = { onFocus }
-                placeholder    = { widthPlaceholder }
-                spellCheck     = { false }
-                value          = { widthValue } />
-        </Column>
-        <Column size = "content">
-            <Text>✕</Text>
-        </Column>
-        <Column>
-            <InputField
-                autoCapitalize = "off"
-                autoComplete   = "off"
-                autoCorrect    = "off"
-                forceHover     = { forceHover }
-                hasError       = { hasError }
-                id             = { `${id}-height` }
-                isDisabled     = { isDisabled }
-                isReadOnly     = { isReadOnly }
-                onBlur         = { onBlur }
-                onChange       = { onChange }
-                onFocus        = { onFocus }
-                placeholder    = { heightPlaceholder }
-                spellCheck     = { false }
-                value          = { heightValue } />
-        </Column>
-    </Row>
-);
 
-DimensionsInput.propTypes = {
-    /**
-     *  Extra CSS class name
-     */
-    className         : PropTypes.string,
-    /**
-     *  Display as hover when required from another component
-     */
-    forceHover        : PropTypes.bool,
-    /**
-     *  Display as error/invalid
-     */
-    hasError          : PropTypes.bool,
-    /**
-    *  Height placeholder text
-    */
-    heightPlaceholder : PropTypes.string,
-    /**
-     * Width input string value
-     */
-    heightValue       : PropTypes.string,
-    /**
-     *  HTML id attribute
-     */
-    id                : PropTypes.string,
-    /**
-     *  Display as disabled
-     */
-    isDisabled        : PropTypes.bool,
-    /**
-     *  Display as read-only
-     */
-    isReadOnly        : PropTypes.bool,
-    /**
-     *  Blur callback function
-     */
-    onBlur            : PropTypes.func,
-    /**
-     *  Input change callback function
-     */
-    onChange          : PropTypes.func,
-    /**
-     *  Focus callback function
-     */
-    onFocus           : PropTypes.func,
-    /**
-     *  Mouse out callback function
-     */
-    onMouseOut        : PropTypes.func,
-    /**
-     *  Mouse over  callback function
-     */
-    onMouseOver       : PropTypes.func,
-    /**
-    *  Width placeholder text
-    */
-    widthPlaceholder  : PropTypes.string,
-    /**
-     * Height input string value
-     */
-    widthValue        : PropTypes.string,
-};
+export default class DimensionsInput extends React.PureComponent
+{
+    static propTypes = {
+        /**
+         *  Extra CSS class name
+         */
+        className         : PropTypes.string,
+        /**
+         *  Display as hover when required from another component
+         */
+        forceHover        : PropTypes.bool,
+        /**
+         *  Display as error/invalid
+         */
+        hasError          : PropTypes.bool,
+        /**
+        *  Height placeholder text
+        */
+        heightPlaceholder : PropTypes.string,
+        /**
+         * Width input string value
+         */
+        heightValue       : PropTypes.string,
+        /**
+         *  HTML id attribute
+         */
+        id                : PropTypes.string,
+        /**
+         *  Display as disabled
+         */
+        isDisabled        : PropTypes.bool,
+        /**
+         *  Display as read-only
+         */
+        isReadOnly        : PropTypes.bool,
+        /**
+         *  Blur callback function
+         */
+        onBlur            : PropTypes.func,
+        /**
+         *  Input change callback function
+         */
+        onChange          : PropTypes.func,
+        /**
+         *  Focus callback function
+         */
+        onFocus           : PropTypes.func,
+        /**
+         *  Mouse out callback function
+         */
+        onMouseOut        : PropTypes.func,
+        /**
+         *  Mouse over  callback function
+         */
+        onMouseOver       : PropTypes.func,
+        /**
+        *  Width placeholder text
+        */
+        widthPlaceholder  : PropTypes.string,
+        /**
+         * Height input string value
+         */
+        widthValue        : PropTypes.string,
+    };
 
-DimensionsInput.defaultProps = {
-    className         : undefined,
-    forceHover        : false,
-    hasError          : false,
-    heightPlaceholder : 'height',
-    heightValue       : '',
-    id                : undefined,
-    isDisabled        : false,
-    isReadOnly        : false,
-    onBlur            : undefined,
-    onChange          : undefined,
-    onFocus           : undefined,
-    onMouseOut        : undefined,
-    onMouseOver       : undefined,
-    widthPlaceholder  : 'width',
-    widthValue        : '',
-};
+    static defaultProps = {
+        className         : undefined,
+        forceHover        : false,
+        hasError          : false,
+        heightPlaceholder : 'height',
+        heightValue       : '',
+        id                : undefined,
+        isDisabled        : false,
+        isReadOnly        : false,
+        onBlur            : undefined,
+        onChange          : undefined,
+        onFocus           : undefined,
+        onMouseOut        : undefined,
+        onMouseOver       : undefined,
+        widthPlaceholder  : 'width',
+        widthValue        : '',
+    };
 
-export default DimensionsInput;
+
+    focusWidth()
+    {
+        this.widthRef.focus();
+    }
+
+    focusHeight()
+    {
+        this.heightRef.focus();
+    }
+
+    render()
+    {
+        const {
+            className,
+            forceHover,
+            hasError,
+            heightPlaceholder,
+            heightValue,
+            id = generateId( 'DimensionsInput' ),
+            isDisabled,
+            isReadOnly,
+            onBlur,
+            onChange,
+            onFocus,
+            onMouseOut,
+            onMouseOver,
+            widthPlaceholder,
+            widthValue,
+        } = this.props;
+
+        return (
+            <Row
+                className     = { className }
+                gutters       = "S"
+                onMouseOut    = { onMouseOut }
+                onMouseOver   = { onMouseOver }
+                verticalAlign = "middle">
+                <Column>
+                    <InputField
+                        autoCapitalize = "off"
+                        autoComplete   = "off"
+                        autoCorrect    = "off"
+                        forceHover     = { forceHover }
+                        hasError       = { hasError }
+                        id             = { `${id}-width` }
+                        isDisabled     = { isDisabled }
+                        isReadOnly     = { isReadOnly }
+                        onBlur         = { onBlur }
+                        onChange       = { onChange }
+                        onFocus        = { onFocus }
+                        placeholder    = { widthPlaceholder }
+                        ref            = { r => this.widthRef = r }
+                        spellCheck     = { false }
+                        value          = { widthValue } />
+                </Column>
+                <Column size = "content">
+                    <Text>✕</Text>
+                </Column>
+                <Column>
+                    <InputField
+                        autoCapitalize = "off"
+                        autoComplete   = "off"
+                        autoCorrect    = "off"
+                        forceHover     = { forceHover }
+                        hasError       = { hasError }
+                        id             = { `${id}-height` }
+                        isDisabled     = { isDisabled }
+                        isReadOnly     = { isReadOnly }
+                        onBlur         = { onBlur }
+                        onChange       = { onChange }
+                        onFocus        = { onFocus }
+                        placeholder    = { heightPlaceholder }
+                        ref            = { r => this.heightRef = r }
+                        spellCheck     = { false }
+                        value          = { heightValue } />
+                </Column>
+            </Row>
+        );
+    }
+}
