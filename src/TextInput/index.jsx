3bb8153db6260032b35cf8ab3186e77e6f4bdd2a
Daniel <daniel.martic@sociomantic.com>
2018-10-05 16:38:14 +0200

    Changed TextInput to pass ref #266

--- src/TextInput/index.jsx
+++ src/TextInput/index.jsx
@@ -14,172 +14,154 @@ import { generateId } from '../utils';
 import InputField     from '../InputField';
 
 
-export default class TextInput extends React.Component
+const TextInput = React.forwardRef( ( {
+    id = generateId( 'TextInput' ),
+    ...props
+}, ref ) =>
+    <InputField { ...props } id = { id } type = "text" ref = { ref } /> );
+
+TextInput.propTypes =
 {
-    static propTypes =
-    {
-        /**
-         *  ARIA properties
-         */
-        aria : PropTypes.objectOf( PropTypes.oneOfType( [
-            PropTypes.bool,
-            PropTypes.number,
-            PropTypes.string,
-        ] ) ),
-        /**
-         *  HTML attribute controlling input auto capitalize
-         */
-        autoCapitalize : PropTypes.oneOf( [
-            'on',
-            'off',
-            'none',
-            'sentences',
-            'words',
-            'characters',
-        ] ),
-        /**
-         *  HTML attribute controlling input auto complete
-         */
-        autoComplete : PropTypes.string,
-        /**
-         *  HTML attribute controlling input auto correct (Safari-specific)
-         */
-        autoCorrect  : PropTypes.oneOf( [ 'on', 'off' ] ),
-        /**
-         *  Extra CSS class name
-         */
-        className    : PropTypes.string,
-        /**
-         *  CSS class map
-         */
-        cssMap       : PropTypes.objectOf( PropTypes.string ),
-        /**
-         *  Display as hover when required from another component
-         */
-        forceHover   : PropTypes.bool,
-        /**
-         *  Display as error/invalid
-         */
-        hasError     : PropTypes.bool,
-        /**
-         *  HTML id attribute
-         */
-        id           : PropTypes.string,
-        /**
-         *  Display as disabled
-         */
-        isDisabled   : PropTypes.bool,
-        /**
-         *  Display as read-only
-         */
-        isReadOnly   : PropTypes.bool,
-        /**
-         *  HTML name attribute
-         */
-        name         : PropTypes.string,
-        /**
-         *  Blur callback function
-         */
-        onBlur       : PropTypes.func,
-        /**
-         *  Input change callback function
-         */
-        onChange     : PropTypes.func,
-        /**
-         *  Input click callback function
-         */
-        onClick      : PropTypes.func,
-        /**
-         *  Focus callback function
-         */
-        onFocus      : PropTypes.func,
-        /**
-         *  Key down callback function
-         */
-        onKeyDown    : PropTypes.func,
-        /**
-         *  Key press callback function
-         */
-        onKeyPress   : PropTypes.func,
-        /**
-         *  Key up callback function
-         */
-        onKeyUp      : PropTypes.func,
-        /**
-         *  Mouse out callback function
-         */
-        onMouseOut   : PropTypes.func,
-        /**
-         *  Mouse over  callback function
-         */
-        onMouseOver  : PropTypes.func,
-        /**
-         *  Placeholder text
-         */
-        placeholder  : PropTypes.string,
-        /**
-         *  HTML attribute controlling input spell check
-         */
-        spellCheck   : PropTypes.bool,
-        /**
-         *  Input text alignment
-         */
-        textAlign    : PropTypes.oneOf( [ 'auto', 'left', 'right' ] ),
-        /**
-         *  Input string value
-         */
-        value        : PropTypes.string,
-    };
+    /**
+     *  ARIA properties
+     */
+    aria : PropTypes.objectOf( PropTypes.oneOfType( [
+        PropTypes.bool,
+        PropTypes.number,
+        PropTypes.string,
+    ] ) ),
+    /**
+     *  HTML attribute controlling input auto capitalize
+     */
+    autoCapitalize : PropTypes.oneOf( [
+        'on',
+        'off',
+        'none',
+        'sentences',
+        'words',
+        'characters',
+    ] ),
+    /**
+     *  HTML attribute controlling input auto complete
+     */
+    autoComplete : PropTypes.string,
+    /**
+     *  HTML attribute controlling input auto correct (Safari-specific)
+     */
+    autoCorrect  : PropTypes.oneOf( [ 'on', 'off' ] ),
+    /**
+     *  Extra CSS class name
+     */
+    className    : PropTypes.string,
+    /**
+     *  CSS class map
+     */
+    cssMap       : PropTypes.objectOf( PropTypes.string ),
+    /**
+     *  Display as hover when required from another component
+     */
+    forceHover   : PropTypes.bool,
+    /**
+     *  Display as error/invalid
+     */
+    hasError     : PropTypes.bool,
+    /**
+     *  HTML id attribute
+     */
+    id           : PropTypes.string,
+    /**
+     *  Display as disabled
+     */
+    isDisabled   : PropTypes.bool,
+    /**
+     *  Display as read-only
+     */
+    isReadOnly   : PropTypes.bool,
+    /**
+     *  HTML name attribute
+     */
+    name         : PropTypes.string,
+    /**
+     *  Blur callback function
+     */
+    onBlur       : PropTypes.func,
+    /**
+     *  Input change callback function
+     */
+    onChange     : PropTypes.func,
+    /**
+     *  Input click callback function
+     */
+    onClick      : PropTypes.func,
+    /**
+     *  Focus callback function
+     */
+    onFocus      : PropTypes.func,
+    /**
+     *  Key down callback function
+     */
+    onKeyDown    : PropTypes.func,
+    /**
+     *  Key press callback function
+     */
+    onKeyPress   : PropTypes.func,
+    /**
+     *  Key up callback function
+     */
+    onKeyUp      : PropTypes.func,
+    /**
+     *  Mouse out callback function
+     */
+    onMouseOut   : PropTypes.func,
+    /**
+     *  Mouse over  callback function
+     */
+    onMouseOver  : PropTypes.func,
+    /**
+     *  Placeholder text
+     */
+    placeholder  : PropTypes.string,
+    /**
+     *  HTML attribute controlling input spell check
+     */
+    spellCheck   : PropTypes.bool,
+    /**
+     *  Input text alignment
+     */
+    textAlign    : PropTypes.oneOf( [ 'auto', 'left', 'right' ] ),
+    /**
+     *  Input string value
+     */
+    value        : PropTypes.string,
+};
 
-    static defaultProps =
-    {
-        aria           : undefined,
-        autoCapitalize : undefined,
-        autoComplete   : undefined,
-        autoCorrect    : undefined,
-        className      : undefined,
-        cssMap         : undefined,
-        forceHover     : false,
-        hasError       : false,
-        id             : undefined,
-        isDisabled     : false,
-        isReadOnly     : false,
-        name           : undefined,
-        onBlur         : undefined,
-        onChange       : undefined,
-        onClick        : undefined,
-        onFocus        : undefined,
-        onKeyDown      : undefined,
-        onKeyPress     : undefined,
-        onKeyUp        : undefined,
-        onMouseOut     : undefined,
-        onMouseOver    : undefined,
-        placeholder    : undefined,
-        spellCheck     : undefined,
-        textAlign      : undefined,
-        value          : '',
-    };
+TextInput.defaultProps =
+{
+    aria           : undefined,
+    autoCapitalize : undefined,
+    autoComplete   : undefined,
+    autoCorrect    : undefined,
+    className      : undefined,
+    cssMap         : undefined,
+    forceHover     : false,
+    hasError       : false,
+    id             : undefined,
+    isDisabled     : false,
+    isReadOnly     : false,
+    name           : undefined,
+    onBlur         : undefined,
+    onChange       : undefined,
+    onClick        : undefined,
+    onFocus        : undefined,
+    onKeyDown      : undefined,
+    onKeyPress     : undefined,
+    onKeyUp        : undefined,
+    onMouseOut     : undefined,
+    onMouseOver    : undefined,
+    placeholder    : undefined,
+    spellCheck     : undefined,
+    textAlign      : undefined,
+    value          : '',
+};
 
-    inputRef = React.createRef();
-
-    focus()
-    {
-        this.inputRef.current.focus();
-    }
-
-    render()
-    {
-        const {
-            id = generateId( 'TextInput' ),
-            ...props
-        } = this.props;
-
-        return (
-            <InputField
-                { ...props }
-                id   = { id }
-                ref  = { this.inputRef }
-                type = "text" />
-        );
-    }
-}
+export default TextInput;
