c5f16128cda3fa25e6cd2bdc0c0d9a5a9a47a3eb
Daniel <daniel.martic@sociomantic.com>
2018-10-05 12:40:54 +0200

    Changed Checkbox to pass ref #266

--- src/Checkbox/index.jsx
+++ src/Checkbox/index.jsx
@@ -14,127 +14,109 @@ import Checkable      from '../proto/Checkable';
 import { generateId } from '../utils';
 import styles         from './checkbox.css';
 
+const Checkbox = React.forwardRef( ( {
+    id = generateId( 'Checkbox' ),
+    ...props
+}, ref ) => (
+    <Checkable { ...props } id = { id } type = "checkbox" ref = { ref } />
+) );
 
-export default class Checkbox extends React.Component
+Checkbox.propTypes =
 {
-    static propTypes =
-    {
-        /**
-         *  Label content (JSX node; overrides label prop)
-         */
-        children    : PropTypes.node,
-        /**
-         *  Extra CSS class name
-         */
-        className   : PropTypes.string,
-        /**
-         *  CSS class map
-         */
-        cssMap      : PropTypes.objectOf( PropTypes.string ),
-        /**
-         * Display as hover when required from another component
-         */
-        forceHover  : PropTypes.bool,
-        /**
-         *  Display as error/invalid
-         */
-        hasError    : PropTypes.bool,
-        /**
-         *  HTML id attribute
-         */
-        id          : PropTypes.string,
-        /**
-         *  Display as checked (controlled input)
-         */
-        isChecked   : PropTypes.bool,
-        /**
-         *  Display as disabled
-         */
-        isDisabled  : PropTypes.bool,
-        /**
-         *  Display as read-only
-         */
-        isReadOnly  : PropTypes.bool,
-        /**
-         *  Label content (string)
-         */
-        label       : PropTypes.string,
-        /**
-         *  Checkbox group name
-         */
-        name        : PropTypes.string,
-        /**
-         *  OnBlur callback function: ( e ) => { ... }
-         */
-        onBlur      : PropTypes.func,
-        /**
-         *  OnClick callback function: ( e ) => { ... }
-         */
-        onClick     : PropTypes.func,
-        /**
-         *  OnChange callback function: ( e ) => { ... }
-         */
-        onChange    : PropTypes.func,
-        /**
-         *  onFocus callback function: ( e ) => { ... }
-         */
-        onFocus     : PropTypes.func,
-        /**
-         *  onMouseOut callback function : ( e ) => { ... }
-         */
-        onMouseOut  : PropTypes.func,
-        /**
-         *  onMouseOver callback function : ( e ) => { ... }
-         */
-        onMouseOver : PropTypes.func,
-        /**
-         *  HTML value attribute
-         */
-        value       : PropTypes.string,
-    };
+    /**
+     *  Label content (JSX node; overrides label prop)
+     */
+    children    : PropTypes.node,
+    /**
+     *  Extra CSS class name
+     */
+    className   : PropTypes.string,
+    /**
+     *  CSS class map
+     */
+    cssMap      : PropTypes.objectOf( PropTypes.string ),
+    /**
+     * Display as hover when required from another component
+     */
+    forceHover  : PropTypes.bool,
+    /**
+     *  Display as error/invalid
+     */
+    hasError    : PropTypes.bool,
+    /**
+     *  HTML id attribute
+     */
+    id          : PropTypes.string,
+    /**
+     *  Display as checked (controlled input)
+     */
+    isChecked   : PropTypes.bool,
+    /**
+     *  Display as disabled
+     */
+    isDisabled  : PropTypes.bool,
+    /**
+     *  Display as read-only
+     */
+    isReadOnly  : PropTypes.bool,
+    /**
+     *  Label content (string)
+     */
+    label       : PropTypes.string,
+    /**
+     *  Checkbox group name
+     */
+    name        : PropTypes.string,
+    /**
+     *  OnBlur callback function: ( e ) => { ... }
+     */
+    onBlur      : PropTypes.func,
+    /**
+     *  OnClick callback function: ( e ) => { ... }
+     */
+    onClick     : PropTypes.func,
+    /**
+     *  OnChange callback function: ( e ) => { ... }
+     */
+    onChange    : PropTypes.func,
+    /**
+     *  onFocus callback function: ( e ) => { ... }
+     */
+    onFocus     : PropTypes.func,
+    /**
+     *  onMouseOut callback function : ( e ) => { ... }
+     */
+    onMouseOut  : PropTypes.func,
+    /**
+     *  onMouseOver callback function : ( e ) => { ... }
+     */
+    onMouseOver : PropTypes.func,
+    /**
+     *  HTML value attribute
+     */
+    value       : PropTypes.string,
+};
 
-    static defaultProps =
-    {
-        children    : undefined,
-        className   : undefined,
-        cssMap      : styles,
-        forceHover  : false,
-        hasError    : false,
-        id          : undefined,
-        isDisabled  : false,
-        isChecked   : false,
-        isReadOnly  : false,
-        label       : undefined,
-        name        : undefined,
-        onBlur      : undefined,
-        onChange    : undefined,
-        onClick     : undefined,
-        onFocus     : undefined,
-        onMouseOut  : undefined,
-        onMouseOver : undefined,
-        value       : undefined,
-    };
+Checkbox.defaultProps =
+{
+    children    : undefined,
+    className   : undefined,
+    cssMap      : styles,
+    forceHover  : false,
+    hasError    : false,
+    id          : undefined,
+    isDisabled  : false,
+    isChecked   : false,
+    isReadOnly  : false,
+    label       : undefined,
+    name        : undefined,
+    onBlur      : undefined,
+    onChange    : undefined,
+    onClick     : undefined,
+    onFocus     : undefined,
+    onMouseOut  : undefined,
+    onMouseOver : undefined,
+    value       : undefined,
+};
 
-    inputRef = React.createRef();
-
-    focus()
-    {
-        this.inputRef.current.focus();
-    }
-
-    render()
-    {
-        const {
-            id = generateId( 'Checkbox' ),
-            ...props
-        } = this.props;
-
-        return (
-            <Checkable
-                { ...props }
-                id   = { id }
-                ref  = { this.inputRef }
-                type = "checkbox" />
-        );
-    }
-}
+export default Checkbox;
