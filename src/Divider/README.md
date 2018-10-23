0a6507a940213986d4d4e0cfbbc2515c35be294c
Daniel Martic <daniel.martic@sociomantic.com>
2018-10-23 11:37:21 +0200

    Updated Divider readme and removed deprecation warning #588

--- src/Divider/README.md
+++ src/Divider/README.md
@@ -1,7 +1,7 @@
 Component Description
 ---------------------
 
-A Nessie Divider is a graphic element placed as in the page as a visual divider, meant for layouting purposes.
+A Nessie Divider is a graphic element placed as in the page as a visual divider, meant for layouting purposes. It can be either horizontal (default) or vertical ( `isVertical` ).
 
 Example Usage
 -------------
--- src/Divider/index.jsx
+++ src/Divider/index.jsx
@@ -12,25 +12,13 @@ import React                from 'react';
 import { buildClassName }   from '../utils';
 
 const Divider = ( { cssMap, className } ) =>
-{
-    if ( !Divider.didWarn )
-    {
-        console.warn( 'Divider is deprecated and will be removed in the next \
-major release' );
-
-        Divider.didWarn = true;
-    }
-
-    return (
-
+    (
         <hr className = { buildClassName( className, cssMap ) } />
-
     );
-};
 
 Divider.defaultProps =
 {
-    cssMap : require( './divider.css' )
+    cssMap : require( './divider.css' ),
 };
 
 export default Divider;
