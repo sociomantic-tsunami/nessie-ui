f83a543b5c006281297322f33f8694536c153c25
Daniel Martic <daniel.martic@sociomantic.com>
2018-10-23 11:41:50 +0200

    Returned deprecation warning #588

--- src/Divider/index.jsx
+++ src/Divider/index.jsx
@@ -12,9 +12,19 @@ import React                from 'react';
 import { buildClassName }   from '../utils';
 
 const Divider = ( { cssMap, className } ) =>
-    (
+{
+    if ( !Divider.didWarn )
+    {
+        console.warn( 'Divider is deprecated and will be removed in the next \
+major release' );
+
+        Divider.didWarn = true;
+    }
+
+    return (
         <hr className = { buildClassName( className, cssMap ) } />
     );
+};
 
 Divider.defaultProps =
 {
