18fe7064ae84aa29d08836066d5dfaf4f00b0a97
Daniel Martic <daniel.martic@sociomantic.com>
2018-11-14 15:30:29 +0100

    Returned and updated old driver methods/tests for CodeEditor #695

--- src/CodeEditor/tests.jsx
+++ src/CodeEditor/tests.jsx
@@ -48,12 +48,14 @@ describe( 'CodeEditor', () =>
 describe( 'CodeEditorDriver', () =>
 {
     let wrapper;
+    let CodeMirror;
     let driver;
 
     beforeEach( () =>
     {
-        wrapper = mount( <CodeEditor /> );
-        driver  = wrapper.driver();
+        wrapper    = mount( <CodeEditor /> );
+        CodeMirror = wrapper.instance().codeMirror;
+        driver     = wrapper.driver();
     } );
 
 
@@ -385,6 +387,146 @@ since it is disabled';
                     expect( onMouseOut ).not.toBeCalled();
                 }
             } );
+        } );
+    } );
+
+
+    describe( 'pressKey( keyCode )', () =>
+    {
+        test( 'should add the character corresponding to keyCode to the \
+end of the code editor’s value', () =>
+        {
+            wrapper.setProps( { value: 'hello' } );
+
+            driver.pressKey( 49 ); // key '1'
+            expect( driver.getInputValue() ).toBe( 'hello1' );
+        } );
+
+        test( 'should call the onChange callback exactly once ', () =>
+        {
+            const onChange = jest.fn();
+            wrapper.setProps( { onChange } );
+
+            driver.pressKey( 50 );
+            expect( onChange ).toBeCalledTimes( 1 );
+        } );
+    } );
+
+
+    describe( 'inputValue( value )', () =>
+    {
+        test( 'should add the value string to end of codeEditor’s value', () =>
+        {
+            wrapper.setProps( { value: 'hello' } );
+
+            driver.inputValue( 'world' );
+            expect( driver.getInputValue() ).toBe( 'helloworld' );
+        } );
+        test( `should call the onChange callback once per
+            printable character in value`, () =>
+        {
+            const onChange = jest.fn();
+            wrapper.setProps( { onChange } );
+
+            driver.inputValue( 'foo' );
+            expect( onChange ).toBeCalledTimes( 3 );
+        } );
+    } );
+
+
+    describe( 'setInputValue( value )', () =>
+    {
+        test( 'should set the input value to "foo"', () =>
+        {
+            driver.setInputValue( 'foo' );
+            expect( CodeMirror.getValue() ).toBe( 'foo' );
+        } );
+
+        test( 'should fire the onChange callback prop once', () =>
+        {
+            const onChange = jest.fn();
+            wrapper.setProps( { onChange } );
+
+            driver.setInputValue( 'foo' );
+            expect( onChange ).toBeCalledTimes( 1 );
+        } );
+
+        test( 'should call onChange with new value as argument', () =>
+        {
+            const onChange = jest.fn();
+            wrapper.setProps( { onChange } );
+
+            driver.setInputValue( 'foo' );
+            expect( onChange ).toBeCalledWith( 'foo' );
+        } );
+
+        test( 'should throw the expected error when component isReadOnly', () =>
+        {
+            wrapper.setProps( { isReadOnly: true } );
+            expect( () => driver.setInputValue( 'foo' ) )
+                .toThrowError( 'CodeEditor cannot simulate setInputValue since \
+it is read only' );
+        } );
+    } );
+
+
+    describe( 'clearInputValue()', () =>
+    {
+        test( 'should set the input value to an empty string', () =>
+        {
+            wrapper.setProps( { defaultValue: 'foo' } );
+
+            driver.clearInputValue();
+            expect( CodeMirror.getValue() ).toBe( '' );
+        } );
+
+        test( 'should throw the expected error when component isReadOnly', () =>
+        {
+            wrapper.setProps( {
+                value      : 'foo',
+                isReadOnly : true,
+            } );
+            expect( () => driver.clearInputValue() )
+                .toThrowError( 'CodeEditor cannot simulate setInputValue since \
+it is read only' );
+        } );
+        test( 'should call the onChange callback exactly once', () =>
+        {
+            const onChange = jest.fn();
+            wrapper.setProps( { onChange } );
+
+            driver.clearInputValue();
+            expect( onChange ).toBeCalledTimes( 1 );
+        } );
+    } );
+
+
+    describe( 'getInputValue()', () =>
+    {
+        test( 'should return the value of the Code Editor input', () =>
+        {
+            wrapper.setProps( { value: 'foo' } );
+            expect( driver.getInputValue() ).toBe( 'foo' );
+        } );
+    } );
+
+
+    describe( 'isReadOnly()', () =>
+    {
+        test( 'should return true if the editor cannot be edited', () =>
+        {
+            wrapper.setProps( { isReadOnly: true } );
+            expect( driver.isReadOnly() ).toBeTruthy();
+        } );
+    } );
+
+
+    describe( 'isDisabled()', () =>
+    {
+        test( 'should return true if the editor is disabled', () =>
+        {
+            wrapper.setProps( { isDisabled: true } );
+            expect( driver.isDisabled() ).toBeTruthy();
         } );
     } );
 } );
