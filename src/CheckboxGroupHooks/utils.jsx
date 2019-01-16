cfd5daf6ac6e1805877ce2dbe66793d6363ee280
Mahmoud.Zakria <mahmoud.zakria@dunnhumby.com>
2019-01-16 16:54:48 +0100

    created checkboxGroup

---
+++ src/CheckboxGroupHooks/utils.jsx
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2018 dunnhumby Germany GmbH.
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the LICENSE file
+ * in the root directory of this source tree.
+ *
+ */
+
+import React        from 'react';
+
+import { Checkbox } from '../index';
+
+/**
+ * ## buildCheckboxesFromValues
+ * Builds an array of Checkbox components from an array of values
+ *
+ * @param   {Array.<String>|Array.<Object>} values  array of values
+ *
+ * @return  {Array.<ReactElement>}
+ *
+ */
+function buildCheckboxesFromValues( values = [], onChange, selectedValues, setValue )
+{
+    const onChangeCB = ( e )=> {
+
+        if(!selectedValues) {
+            return;
+        }
+
+        const { target } = e;
+        if(target.checked){
+            if(selectedValues.indexOf(target.value) === -1){
+                const result = [...selectedValues, target.value];
+                setValue( result );
+            }
+        } else {
+            const index = selectedValues.indexOf(target.value);
+            selectedValues.splice(index, 1);
+            setValue( selectedValues );
+        }
+    }
+
+    return values.map( value =>
+    {
+        const props = typeof value === 'object' ? value : {
+            id    : value,
+            label : value,
+            value,
+            onChange: onChange || onChangeCB
+        };
+
+        return <Checkbox { ...props } />;
+    } );
+}
+
+export { buildCheckboxesFromValues };
