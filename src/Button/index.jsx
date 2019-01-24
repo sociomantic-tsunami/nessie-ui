e9b14e489128f576cfa3053c5171372fd8aa826a
Mahmoud.Zakria <mahmoud.zakria@dunnhumby.com>
2019-01-24 14:05:09 +0100

    converted to hooks #767

--- src/Button/index.jsx
+++ src/Button/index.jsx
@@ -7,203 +7,203 @@
  *
  */
 
-import React                        from 'react';
-import PropTypes                    from 'prop-types';
+import React, {
+    useContext,
+    useImperativeHandle,
+    useRef,
+    forwardRef
+} from 'react';
 
-import { Icon, Spinner }            from '..';
+import PropTypes                                         from 'prop-types';
+import { Icon, Spinner }                                 from '..';
 
-import { attachEvents, generateId } from '../utils';
-import ThemeContext                 from '../Theming/ThemeContext';
-import { createCssMap }             from '../Theming';
+import { attachEvents, generateId }                      from '../utils';
+import ThemeContext                                      from '../Theming/ThemeContext';
+import { createCssMap }                                  from '../Theming';
 
 
-export default class Button extends React.Component
+const Button = forwardRef( ( props, ref ) =>
 {
-    static contextType = ThemeContext;
 
-    static propTypes =
-    {
-        /**
-         *  Label text (React node; overrides label prop)
-         */
-        children     : PropTypes.node,
-        /**
-         *  CSS class map
-         */
-        cssMap       : PropTypes.objectOf( PropTypes.string ),
-        /**
-         *  Icon position relative to label
-         */
-        iconPosition : PropTypes.oneOf( [ 'left', 'right' ] ),
-        /**
-         *  Icon type to display
-         */
-        iconType     : PropTypes.oneOf( [
-            'account',
-            'add-circle',
-            'add',
-            'alert',
-            'approved',
-            'arrow-down',
-            'arrow-up',
-            'arrow',
-            'bell',
-            'board',
-            'calendar',
-            'close-circle',
-            'close-thick',
-            'close',
-            'dash',
-            'dashboard',
-            'deactivated',
-            'declined',
-            'delete',
-            'down',
-            'download',
-            'duplicate',
-            'edit-circle',
-            'edit',
-            'ended',
-            'error',
-            'file',
-            'graph',
-            'hide',
-            'info',
-            'inspect',
-            'left',
-            'lightbulb',
-            'link',
-            'loader',
-            'megaphone',
-            'options',
-            'paused',
-            'pending',
-            'preview',
-            'puzzle-piece',
-            'reset',
-            'right',
-            'search',
-            'show',
-            'sociomantic',
-            'star-stroke',
-            'star',
-            'swap',
-            'table',
-            'up',
-            'upload',
-            'validation',
-            'none',
-        ] ),
-        /**
-         * Component identifier
-         */
-        id          : PropTypes.string,
-        /**
-         *  Display as disabled
-         */
-        isDisabled  : PropTypes.bool,
-        /**
-         *  Display as loading
-         */
-        isLoading   : PropTypes.bool,
-        /**
-         *  Label text
-         */
-        label       : PropTypes.string,
-        /**
-         *  click callback function: ( { id } ) => ...
-         */
-        onClick     : PropTypes.func,
-        /**
-         *  mouse out callback function: ( { id } ) => ...
-         */
-        onMouseOut  : PropTypes.func,
-        /**
-         *  mouse over callback function: ( { id } ) => ...
-         */
-        onMouseOver : PropTypes.func,
-        /**
-         *  Role/style
-         */
-        role        : PropTypes.oneOf( [
-            'default',
-            'secondary',
-            'subtle',
-            'promoted',
-            'critical',
-            'control',
-        ] ),
-    };
+    const context = useContext( ThemeContext );
+    const buttonRef = useRef();
 
-    static defaultProps =
-    {
-        children     : undefined,
-        cssMap       : undefined,
-        iconPosition : 'left',
-        iconType     : 'none',
-        id           : undefined,
-        isDisabled   : false,
-        isLoading    : false,
-        label        : undefined,
-        onClick      : undefined,
-        onMouseOut   : undefined,
-        onMouseOver  : undefined,
-        role         : 'default',
-    };
+    useImperativeHandle( ref, () => ( {
+        focus : () =>
+        {
+            buttonRef.current.focus();
+        }
+    } ) );
 
-    static displayName = 'Button';
+    const {
+        children,
+        iconType,
+        cssMap = createCssMap( context.Button, props ),
+        id = generateId( 'Button' ),
+        isDisabled,
+        isLoading,
+        label,
+    } = props;
 
-    buttonRef = React.createRef();
 
-    constructor( props )
-    {
-        super();
-        this.state = { id: props.id || generateId( 'Button' ) };
-    }
-
-    focus()
-    {
-        this.buttonRef.current.focus();
-    }
-
-    render()
-    {
-        const {
-            children,
-            cssMap = createCssMap( this.context.Button, this.props ),
-            iconType,
-            isDisabled,
-            isLoading,
-            label,
-        } = this.props;
-
-        const { id } = this.state;
-
-        return (
-            <button
-                { ...attachEvents( this.props ) }
-                className = { cssMap.main }
-                disabled  = { isDisabled }
-                id        = { id }
-                ref       = { this.buttonRef }
-                type      = "button">
-                <div className = { cssMap.content }>
-                    { ( iconType && iconType !== 'none' ) &&
-                        <Icon
-                            className = { cssMap.icon }
-                            size      = "S"
-                            type      = { iconType } />
-                    }
-                    <div className = { cssMap.label }>
-                        { children || label }
-                    </div>
-                </div>
-                { ( isLoading && !isDisabled ) &&
-                    <div className = { cssMap.loadingOverlay }>
-                        <Spinner className = { cssMap.spinner } />
-                    </div>
+    return (
+        <button
+            { ...attachEvents( props ) }
+            className = { cssMap.main }
+            disabled  = { isDisabled }
+            id        = { id }
+            ref       = { buttonRef }
+            type      = "button">
+            <div className = { cssMap.content }>
+                { ( iconType && iconType !== 'none' ) &&
+                    <Icon
+                        className = { cssMap.icon }
+                        size      = "S"
+                        type      = { iconType } />
                 }
-            </button>
-        );
-    }
-}
+                <div className = { cssMap.label }>
+                    { children || label }
+                </div>
+            </div>
+            { ( isLoading && !isDisabled ) &&
+                <div className = { cssMap.loadingOverlay }>
+                    <Spinner className = { cssMap.spinner } />
+                </div>
+            }
+        </button>
+    );
+} );
+
+Button.displayName = 'Button';
+
+Button.propTypes =
+{
+    /**
+     *  Label text (React node; overrides label prop)
+     */
+    children     : PropTypes.node,
+    /**
+     *  CSS class map
+     */
+    cssMap       : PropTypes.objectOf( PropTypes.string ),
+    /**
+     *  Icon position relative to label
+     */
+    iconPosition : PropTypes.oneOf( [ 'left', 'right' ] ),
+    /**
+     *  Icon type to display
+     */
+    iconType     : PropTypes.oneOf( [
+        'account',
+        'add-circle',
+        'add',
+        'alert',
+        'approved',
+        'arrow-down',
+        'arrow-up',
+        'arrow',
+        'bell',
+        'board',
+        'calendar',
+        'close-circle',
+        'close-thick',
+        'close',
+        'dash',
+        'dashboard',
+        'deactivated',
+        'declined',
+        'delete',
+        'down',
+        'download',
+        'duplicate',
+        'edit-circle',
+        'edit',
+        'ended',
+        'error',
+        'file',
+        'graph',
+        'hide',
+        'info',
+        'inspect',
+        'left',
+        'lightbulb',
+        'link',
+        'loader',
+        'megaphone',
+        'options',
+        'paused',
+        'pending',
+        'preview',
+        'puzzle-piece',
+        'reset',
+        'right',
+        'search',
+        'show',
+        'sociomantic',
+        'star-stroke',
+        'star',
+        'swap',
+        'table',
+        'up',
+        'upload',
+        'validation',
+        'none',
+    ] ),
+    /**
+     * Component identifier
+     */
+    id          : PropTypes.string,
+    /**
+     *  Display as disabled
+     */
+    isDisabled  : PropTypes.bool,
+    /**
+     *  Display as loading
+     */
+    isLoading   : PropTypes.bool,
+    /**
+     *  Label text
+     */
+    label       : PropTypes.string,
+    /**
+     *  click callback function: ( { id } ) => ...
+     */
+    onClick     : PropTypes.func,
+    /**
+     *  mouse out callback function: ( { id } ) => ...
+     */
+    onMouseOut  : PropTypes.func,
+    /**
+     *  mouse over callback function: ( { id } ) => ...
+     */
+    onMouseOver : PropTypes.func,
+    /**
+     *  Role/style
+     */
+    role        : PropTypes.oneOf( [
+        'default',
+        'secondary',
+        'subtle',
+        'promoted',
+        'critical',
+        'control',
+    ] ),
+};
+
+Button.defaultProps =
+{
+    children     : undefined,
+    cssMap       : undefined,
+    iconPosition : 'left',
+    iconType     : 'none',
+    id           : undefined,
+    isDisabled   : false,
+    isLoading    : false,
+    label        : undefined,
+    onClick      : undefined,
+    onMouseOut   : undefined,
+    onMouseOver  : undefined,
+    role         : 'default',
+};
+
+export default Button;
