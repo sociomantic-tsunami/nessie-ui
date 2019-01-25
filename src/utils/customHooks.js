9612fd313af585524ca8efa3f7e7fded78b182d5
Daniel Martic <daniel.martic@sociomantic.com>
2019-01-25 15:23:44 +0100

    Removed createCssMap, created customHooks

---
+++ src/utils/customHooks.js
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2019 dunnhumby Germany GmbH.
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the LICENSE file
+ * in the root directory of this source tree.
+ *
+ */
+
+import { useContext }     from 'react';
+
+import { ThemeProvider }  from '../Theming';
+
+/**
+ * Builds class names from theme
+ *
+ * @param {String} displayName component's displayName
+ * @param {Object} props component's props
+ *
+ * @return {Object} cssMap containing component's styles
+ */
+function useTheme( displayName, props )
+{
+    const { [ displayName ] : theme } = useContext( ThemeProvider );
+
+    return props.cssMap ||
+        ( typeof theme === 'function' ? theme( props ) : theme );
+}
+
+export default useTheme;
