dd452fb9706726efefab7539cb91d87aa81c07f5
Mahmoud.Zakria <mahmoud.zakria@dunnhumby.com>
2019-01-30 12:04:08 +0100

    remove readyOnly, fix lint error

--- src/CurrencyInput/index.jsx
+++ src/CurrencyInput/index.jsx
@@ -20,113 +20,112 @@ const currencyFormat = ( number, currency, language = navigator.language ) =>
 
 export default class CurrencyInput extends React.Component
 {
-
     static propTypes =
     {
         /**
          *  Extra CSS class name
          */
-        className    : PropTypes.string,
+        className   : PropTypes.string,
         /**
          *  CSS class map
          */
-        cssMap       : PropTypes.objectOf( PropTypes.string ),
+        cssMap      : PropTypes.objectOf( PropTypes.string ),
         /**
          *  Currencies
          */
-        currency     : PropTypes.oneOf( [ 'USD', 'EUR', 'GBP' ] ),
+        currency    : PropTypes.oneOf( [ 'USD', 'EUR', 'GBP' ] ),
         /**
          *  Display as error/invalid
          */
-        hasError     : PropTypes.bool,
+        hasError    : PropTypes.bool,
         /**
          *  HTML id attribute
          */
-        id           : PropTypes.string,
+        id          : PropTypes.string,
         /**
          *  Display as disabled
          */
-        isDisabled   : PropTypes.bool,
+        isDisabled  : PropTypes.bool,
         /**
          *  Display as read-only
          */
-        isReadOnly   : PropTypes.bool,
+        isReadOnly  : PropTypes.bool,
         /**
          *  Blur callback function
          */
-        onBlur       : PropTypes.func,
+        onBlur      : PropTypes.func,
         /**
          *  Input change callback function
          */
-        onChange     : PropTypes.func,
+        onChange    : PropTypes.func,
         /**
          *  Input click callback function
          */
-        onClick      : PropTypes.func,
+        onClick     : PropTypes.func,
         /**
          *  Icon click callback function
          */
-        onClickIcon  : PropTypes.func,
+        onClickIcon : PropTypes.func,
         /**
          *  Focus callback function
          */
-        onFocus      : PropTypes.func,
+        onFocus     : PropTypes.func,
         /**
          *  Key down callback function
          */
-        onKeyDown    : PropTypes.func,
+        onKeyDown   : PropTypes.func,
         /**
          *  Key press callback function
          */
-        onKeyPress   : PropTypes.func,
+        onKeyPress  : PropTypes.func,
         /**
          *  Key up callback function
          */
-        onKeyUp      : PropTypes.func,
+        onKeyUp     : PropTypes.func,
         /**
          *  Mouse out callback function
          */
-        onMouseOut   : PropTypes.func,
+        onMouseOut  : PropTypes.func,
         /**
          *  Mouse over  callback function
          */
-        onMouseOver  : PropTypes.func,
+        onMouseOver : PropTypes.func,
         /**
          *  Placeholder text
          */
-        placeholder  : PropTypes.string,
+        placeholder : PropTypes.string,
         /**
          *  Input text alignment
          */
-        textAlign    : PropTypes.oneOf( [ 'left', 'right' ] ),
+        textAlign   : PropTypes.oneOf( [ 'left', 'right' ] ),
         /**
          *  Input string value
          */
-        value        : PropTypes.string,
+        value       : PropTypes.string,
     };
 
     static defaultProps =
     {
-        className      : undefined,
-        cssMap         : undefined,
-        currency       : undefined,
-        hasError       : false,
-        id             : undefined,
-        isDisabled     : false,
-        isReadOnly     : false,
-        onBlur         : undefined,
-        onChange       : undefined,
-        onClick        : undefined,
-        onClickIcon    : undefined,
-        onFocus        : undefined,
-        onKeyDown      : undefined,
-        onKeyPress     : undefined,
-        onKeyUp        : undefined,
-        onMouseOut     : undefined,
-        onMouseOver    : undefined,
-        placeholder    : undefined,
-        textAlign      : 'left',
-        value          : '',
+        className   : undefined,
+        cssMap      : undefined,
+        currency    : undefined,
+        hasError    : false,
+        id          : undefined,
+        isDisabled  : false,
+        isReadOnly  : false,
+        onBlur      : undefined,
+        onChange    : undefined,
+        onClick     : undefined,
+        onClickIcon : undefined,
+        onFocus     : undefined,
+        onKeyDown   : undefined,
+        onKeyPress  : undefined,
+        onKeyUp     : undefined,
+        onMouseOut  : undefined,
+        onMouseOver : undefined,
+        placeholder : undefined,
+        textAlign   : 'left',
+        value       : '',
     };
 
 
@@ -136,7 +135,7 @@ export default class CurrencyInput extends React.Component
     {
         super( props );
         this.state = {
-            valueFormatted : ''
+            valueFormatted : '',
         };
         this.handleBlur   = this.handleBlur.bind( this );
         this.handleChange = this.handleChange.bind( this );
@@ -146,7 +145,7 @@ export default class CurrencyInput extends React.Component
     {
         const newVal = Number( this.state.valueFormatted.replace( /[^0-9\.-]/g, '' ) );
         this.setState( {
-            valueFormatted: newVal
+            valueFormatted : newVal,
         } );
         if ( typeof this.props.onBlur === 'function' )
         {
@@ -165,11 +164,9 @@ export default class CurrencyInput extends React.Component
 
     render()
     {
-
         const {
             currency,
             id = generateId( 'CurrencyInput' ),
-            isReadOnly,
         } = this.props;
 
         return (
@@ -180,7 +177,6 @@ export default class CurrencyInput extends React.Component
                 autoCorrect    = "off"
                 spellCheck     = { false }
                 id             = { id }
-                readOnly       = { isReadOnly }
                 onBlur         = { this.handleBlur }
                 onChange       = { this.handleChange }
                 value          = { currencyFormat( this.state.valueFormatted, currency ) } />
