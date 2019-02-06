9a85267c3e3a280e8a7a5a65daca3c2b5b93e7cf
Damian Rodriguez <damian.rodriguez@sociomantic.com>
2019-02-06 16:24:32 +0100

    convert PopperWrapper in a class component

--- src/PopperWrapper/index.jsx
+++ src/PopperWrapper/index.jsx
@@ -9,126 +9,127 @@
 
 /* global document */
 
-import React                                      from 'react';
+import React, { Component }                       from 'react';
 import ReactDOM                                   from 'react-dom';
 import { Manager, Reference, Popper }             from 'react-popper';
 import PropTypes                                  from 'prop-types';
 
-const PopperWrapper = props =>
+export default class PopperWrapper extends Component
 {
-    const {
-        children,
-        container,
-        isVisible,
-        popper,
-        popperOffset,
-        popperPosition,
-        popperWidth,
-    } = props;
+    static propTypes =
+    {
+        /**
+         *  Reference node to attach popper
+         */
+        children       : PropTypes.node,
+        /**
+         *  id of the DOM element used as container
+         */
+        container      : PropTypes.string,
+        /**
+         *  Show / Hide popper
+         */
+        isVisible      : PropTypes.bool,
+        /**
+         *  Popper content node
+         */
+        popper         : PropTypes.node,
+        /**
+         *  Popper offset
+         */
+        popperOffset   : PropTypes.oneOf( [ 'S', 'M', 'L', 'XL', 'none' ] ),
+        /**
+         *  Popper position
+         */
+        popperPosition : PropTypes.oneOf( [
+            'auto',
+            'auto-start',
+            'auto-end',
+            'top',
+            'top-start',
+            'top-end',
+            'bottom',
+            'bottom-start',
+            'bottom-end',
+            'left',
+            'left-start',
+            'left-end',
+            'right',
+            'right-start',
+            'right-end',
+        ] ),
+        /**
+         *  Popper width
+         */
+        popperWidth : PropTypes.number,
+    }
 
-    const offset = {
-        'S'    : '8px',
-        'M'    : '16px',
-        'L'    : '24px',
-        'XL'   : '32px',
-        'none' : undefined,
-    }[ popperOffset ];
+    static defaultProps =
+    {
+        children       : undefined,
+        container      : undefined,
+        isVisible      : false,
+        popper         : undefined,
+        popperOffset   : 'none',
+        popperPosition : 'auto',
+        popperWidth    : undefined,
+    }
 
-    return (
-        <Manager>
-            <Reference>
-                { ( { ref } ) => (
-                    <div ref = { ref }>
-                        { children }
-                    </div>
+    static displayName = 'PopperWrapper';
+
+    render()
+    {
+        const {
+            children,
+            container,
+            isVisible,
+            popper,
+            popperOffset,
+            popperPosition,
+            popperWidth,
+        } = this.props;
+
+        const offset = {
+            'S'    : '8px',
+            'M'    : '16px',
+            'L'    : '24px',
+            'XL'   : '32px',
+            'none' : undefined,
+        }[ popperOffset ];
+
+        return (
+            <Manager>
+                <Reference>
+                    { ( { ref } ) => (
+                        <div ref = { ref }>
+                            { children }
+                        </div>
+                    ) }
+                </Reference>
+                { isVisible && ReactDOM.createPortal(
+                    <Popper
+                        placement = { popperPosition }
+                        modifiers = { offset ? {
+                            offset : {
+                                offset : `0, ${offset}`,
+                            },
+                        } : offset }>
+                        { ( { ref, style } ) =>
+                            (
+                                <div
+                                    ref   = { ref }
+                                    style = { popperWidth ? {
+                                        'width' : `${popperWidth}px`,
+                                        ...style,
+                                    } : style }>
+                                    { popper }
+                                </div>
+                            ) }
+                    </Popper>,
+                    container ? document.querySelector( container ) :
+                        document.body,
                 ) }
-            </Reference>
-            { isVisible && ReactDOM.createPortal(
-                <Popper
-                    placement = { popperPosition }
-                    modifiers = { offset ? {
-                        offset : {
-                            offset : `0, ${offset}`,
-                        },
-                    } : offset }>
-                    { ( { ref, style } ) =>
-                        (
-                            <div
-                                ref   = { ref }
-                                style = { popperWidth ? {
-                                    'width' : `${popperWidth}px`,
-                                    ...style,
-                                } : style }>
-                                { popper }
-                            </div>
-                        ) }
-                </Popper>,
-                container ? document.querySelector( container ) :
-                    document.body,
-            ) }
-        </Manager>
-    );
-};
-
-PopperWrapper.propTypes =
-{
-    /**
-     *  Reference node to attach popper
-     */
-    children       : PropTypes.node,
-    /**
-     *  id of the DOM element used as container
-     */
-    container      : PropTypes.string,
-    /**
-     *  Show / Hide popper
-     */
-    isVisible      : PropTypes.bool,
-    /**
-     *  Popper content node
-     */
-    popper         : PropTypes.node,
-    /**
-     *  Popper offset
-     */
-    popperOffset   : PropTypes.oneOf( [ 'S', 'M', 'L', 'XL', 'none' ] ),
-    /**
-     *  Popper position
-     */
-    popperPosition : PropTypes.oneOf( [
-        'auto',
-        'auto-start',
-        'auto-end',
-        'top',
-        'top-start',
-        'top-end',
-        'bottom',
-        'bottom-start',
-        'bottom-end',
-        'left',
-        'left-start',
-        'left-end',
-        'right',
-        'right-start',
-        'right-end',
-    ] ),
-    /**
-     *  Popper width
-     */
-    popperWidth : PropTypes.number,
-};
-
-PopperWrapper.defaultProps =
-{
-    children       : undefined,
-    container      : undefined,
-    isVisible      : false,
-    popper         : undefined,
-    popperOffset   : 'none',
-    popperPosition : 'auto',
-    popperWidth    : undefined,
-};
-
-PopperWrapper.displayName = 'PopperWrapper';
-
-export default PopperWrapper;
+            </Manager>
+        );
+    }
+}
