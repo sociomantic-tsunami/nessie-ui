1e49693f27ac451f7cdf875ad72896ecf6ac9ee0
Mahmoud.Zakria <mahmoud.zakria@dunnhumby.com>
2019-02-13 19:52:25 +0100

    convert to hooks

--- src/TextInputWithIcon/index.jsx
+++ src/TextInputWithIcon/index.jsx
@@ -14,248 +14,246 @@ import { attachEvents, generateId } from '../utils';
 
 import { IconButton, TextInput }    from '..';
 
-import ThemeContext                 from '../Theming/ThemeContext';
-import { createCssMap }             from '../Theming';
+import { useTheme }                 from '../Theming';
 
+const componentName = 'TextInputWithIcon';
 
-export default class TextInputWithIcon extends React.Component
+const TextInputWithIcon = ( props ) =>
 {
-    static contextType = ThemeContext;
+    const cssMap = useTheme( componentName, props );
 
-    static propTypes =
+    const {
+        aria,
+        autoCapitalize,
+        autoComplete,
+        autoCorrect,
+        forceHover,
+        hasError,
+        iconButtonIsDisabled,
+        iconPosition,
+        iconType,
+        id = generateId( componentName ),
+        inputRef,
+        inputType,
+        isDisabled,
+        isReadOnly,
+        name,
+        onChangeInput,
+        onClickIcon,
+        onKeyDownInput,
+        placeholder,
+        spellCheck,
+        textAlign,
+        value,
+    } = props;
+
+    let alignText = textAlign;
+
+    if ( textAlign === 'auto' )
     {
-        /**
-         *  ARIA properties
-         */
-        aria : PropTypes.objectOf( PropTypes.oneOfType( [
-            PropTypes.bool,
-            PropTypes.number,
-            PropTypes.string,
-        ] ) ),
-        /**
-         *  HTML attribute controlling input auto capitalize
-         */
-        autoCapitalize : PropTypes.oneOf( [
-            'on',
-            'off',
-            'none',
-            'sentences',
-            'words',
-            'characters',
-        ] ),
-        /**
-         *  HTML attribute controlling input auto complete
-         */
-        autoComplete         : PropTypes.string,
-        /**
-         *  HTML attribute controlling input auto correct (Safari-specific)
-         */
-        autoCorrect          : PropTypes.oneOf( [ 'on', 'off' ] ),
-        /**
-         *  Extra CSS class name
-         */
-        className            : PropTypes.string,
-        /**
-         *  CSS class map
-         */
-        cssMap               : PropTypes.objectOf( PropTypes.string ),
-        /**
-         *  Display as hover when required from another component
-         */
-        forceHover           : PropTypes.bool,
-        /**
-         *  Display as error/invalid
-         */
-        hasError             : PropTypes.bool,
-        /**
-         *  Display Button icon as disabled
-         */
-        iconButtonIsDisabled : PropTypes.bool,
-        /**
-         *  Alignment of the icon
-         */
-        iconPosition         : PropTypes.oneOf( [ 'left', 'right' ] ),
-        /**
-         *  Icon type to display (see https://feathericons.com/)
-         */
-        iconType             : PropTypes.string,
-        /**
-         *  Component id
-         */
-        id                   : PropTypes.string,
-        /**
-         *  Callback that receives the native <input>: ( ref ) => { ... }
-         */
-        inputRef             : PropTypes.func,
-        /**
-         *  HTML input type
-         */
-        inputType            : PropTypes.oneOf( [ 'text', 'password' ] ),
-        /**
-         *  Display as disabled
-         */
-        isDisabled           : PropTypes.bool,
-        /**
-         *  Display as read-only
-         */
-        isReadOnly           : PropTypes.bool,
-        /**
-         *  HTML name attribute
-         */
-        name                 : PropTypes.string,
-        /**
-         *  Blur callback function
-         */
-        onBlur               : PropTypes.func,
-        /**
-         *  Input change callback function
-         */
-        onChangeInput        : PropTypes.func,
-        /**
-         *  Click callback function
-         */
-        onClick              : PropTypes.func,
-        /**
-         *  Icon click callback function
-         */
-        onClickIcon          : PropTypes.func,
-        /**
-         *  Focus callback function
-         */
-        onFocus              : PropTypes.func,
-        /**
-         *  Input key down callback function
-         */
-        onKeyDownInput       : PropTypes.func,
-        /**
-         *  Mouse out callback function
-         */
-        onMouseOut          : PropTypes.func,
-        /**
-         *  Mouse over  callback function
-         */
-        onMouseOver          : PropTypes.func,
-        /**
-         *  Placeholder text
-         */
-        placeholder          : PropTypes.string,
-        /**
-         *  HTML attribute controlling input spell check
-         */
-        spellCheck           : PropTypes.bool,
-        /**
-         *  Input text alignment
-         */
-        textAlign            : PropTypes.oneOf( [ 'auto', 'left', 'right' ] ),
-        /**
-         *  Input string value
-         */
-        value                : PropTypes.string,
-    };
-
-    static defaultProps =
-    {
-        aria                 : undefined,
-        autoCapitalize       : undefined,
-        autoComplete         : undefined,
-        autoCorrect          : undefined,
-        className            : undefined,
-        cssMap               : undefined,
-        forceHover           : false,
-        hasError             : false,
-        iconButtonIsDisabled : false,
-        iconPosition         : 'right',
-        iconType             : 'none',
-        id                   : undefined,
-        inputRef             : undefined,
-        inputType            : 'text',
-        isDisabled           : false,
-        isReadOnly           : false,
-        name                 : undefined,
-        onBlur               : undefined,
-        onChangeInput        : undefined,
-        onClick              : undefined,
-        onClickIcon          : undefined,
-        onFocus              : undefined,
-        onKeyDownInput       : undefined,
-        onMouseOut           : undefined,
-        onMouseOver          : undefined,
-        placeholder          : undefined,
-        spellCheck           : undefined,
-        textAlign            : 'auto',
-        value                : '',
-    };
-
-    static displayName = 'TextInputWithIcon';
-
-    render()
-    {
-        const {
-            aria,
-            autoCapitalize,
-            autoComplete,
-            autoCorrect,
-            cssMap = createCssMap( this.context.TextInputWithIcon, this.props ),
-            forceHover,
-            hasError,
-            iconButtonIsDisabled,
-            iconPosition,
-            iconType,
-            id = generateId( 'TextInputWithIcon' ),
-            inputRef,
-            inputType,
-            isDisabled,
-            isReadOnly,
-            name,
-            onChangeInput,
-            onClickIcon,
-            onKeyDownInput,
-            placeholder,
-            spellCheck,
-            textAlign,
-            value,
-        } = this.props;
-
-        let alignText = textAlign;
-
-        if ( textAlign === 'auto' )
-        {
-            alignText = ( iconType !== 'none' && iconPosition === 'left' ) ?
-                'right' : 'left';
-        }
-
-        return (
-            <div { ...attachEvents( this.props ) } className = { cssMap.main }>
-                <TextInput
-                    aria           = { aria }
-                    autocapitalize = { autoCapitalize }
-                    autoComplete   = { autoComplete }
-                    autoCorrect    = { autoCorrect }
-                    className      = { cssMap.input }
-                    forceHover     = { forceHover }
-                    hasError       = { hasError }
-                    id             = { id }
-                    isDisabled     = { isDisabled }
-                    isReadOnly     = { isReadOnly }
-                    name           = { name }
-                    onChange       = { onChangeInput }
-                    onKeyDown      = { onKeyDownInput }
-                    placeholder    = { placeholder }
-                    ref            = { inputRef }
-                    spellcheck     = { spellCheck }
-                    textAlign      = { alignText }
-                    type           = { inputType }
-                    value          = { value } />
-                { ( iconType && iconType !== 'none' ) &&
-                    <IconButton
-                        className   = { cssMap.icon }
-                        hasError    = { hasError }
-                        iconType    = { iconType }
-                        isDisabled  = { isDisabled || iconButtonIsDisabled }
-                        isFocusable = { false }
-                        onClick     = { onClickIcon } />
-                }
-            </div>
-        );
+        alignText = ( iconType !== 'none' && iconPosition === 'left' ) ?
+            'right' : 'left';
     }
-}
+
+    return (
+        <div { ...attachEvents( props ) } className = { cssMap.main }>
+            <TextInput
+                aria           = { aria }
+                autocapitalize = { autoCapitalize }
+                autoComplete   = { autoComplete }
+                autoCorrect    = { autoCorrect }
+                className      = { cssMap.input }
+                forceHover     = { forceHover }
+                hasError       = { hasError }
+                id             = { id }
+                isDisabled     = { isDisabled }
+                isReadOnly     = { isReadOnly }
+                name           = { name }
+                onChange       = { onChangeInput }
+                onKeyDown      = { onKeyDownInput }
+                placeholder    = { placeholder }
+                ref            = { inputRef }
+                spellcheck     = { spellCheck }
+                textAlign      = { alignText }
+                type           = { inputType }
+                value          = { value } />
+            { ( iconType && iconType !== 'none' ) &&
+                <IconButton
+                    className   = { cssMap.icon }
+                    hasError    = { hasError }
+                    iconType    = { iconType }
+                    isDisabled  = { isDisabled || iconButtonIsDisabled }
+                    isFocusable = { false }
+                    onClick     = { onClickIcon } />
+            }
+        </div>
+    );
+};
+
+TextInputWithIcon.propTypes =
+{
+    /**
+     *  ARIA properties
+     */
+    aria : PropTypes.objectOf( PropTypes.oneOfType( [
+        PropTypes.bool,
+        PropTypes.number,
+        PropTypes.string,
+    ] ) ),
+    /**
+     *  HTML attribute controlling input auto capitalize
+     */
+    autoCapitalize : PropTypes.oneOf( [
+        'on',
+        'off',
+        'none',
+        'sentences',
+        'words',
+        'characters',
+    ] ),
+    /**
+     *  HTML attribute controlling input auto complete
+     */
+    autoComplete         : PropTypes.string,
+    /**
+     *  HTML attribute controlling input auto correct (Safari-specific)
+     */
+    autoCorrect          : PropTypes.oneOf( [ 'on', 'off' ] ),
+    /**
+     *  Extra CSS class name
+     */
+    className            : PropTypes.string,
+    /**
+     *  CSS class map
+     */
+    cssMap               : PropTypes.objectOf( PropTypes.string ),
+    /**
+     *  Display as hover when required from another component
+     */
+    forceHover           : PropTypes.bool,
+    /**
+     *  Display as error/invalid
+     */
+    hasError             : PropTypes.bool,
+    /**
+     *  Display Button icon as disabled
+     */
+    iconButtonIsDisabled : PropTypes.bool,
+    /**
+     *  Alignment of the icon
+     */
+    iconPosition         : PropTypes.oneOf( [ 'left', 'right' ] ),
+    /**
+     *  Icon type to display (see https://feathericons.com/)
+     */
+    iconType             : PropTypes.string,
+    /**
+     *  Component id
+     */
+    id                   : PropTypes.string,
+    /**
+     *  Callback that receives the native <input>: ( ref ) => { ... }
+     */
+    inputRef             : PropTypes.func,
+    /**
+     *  HTML input type
+     */
+    inputType            : PropTypes.oneOf( [ 'text', 'password' ] ),
+    /**
+     *  Display as disabled
+     */
+    isDisabled           : PropTypes.bool,
+    /**
+     *  Display as read-only
+     */
+    isReadOnly           : PropTypes.bool,
+    /**
+     *  HTML name attribute
+     */
+    name                 : PropTypes.string,
+    /**
+     *  Blur callback function
+     */
+    onBlur               : PropTypes.func,
+    /**
+     *  Input change callback function
+     */
+    onChangeInput        : PropTypes.func,
+    /**
+     *  Click callback function
+     */
+    onClick              : PropTypes.func,
+    /**
+     *  Icon click callback function
+     */
+    onClickIcon          : PropTypes.func,
+    /**
+     *  Focus callback function
+     */
+    onFocus              : PropTypes.func,
+    /**
+     *  Input key down callback function
+     */
+    onKeyDownInput       : PropTypes.func,
+    /**
+     *  Mouse out callback function
+     */
+    onMouseOut           : PropTypes.func,
+    /**
+     *  Mouse over  callback function
+     */
+    onMouseOver          : PropTypes.func,
+    /**
+     *  Placeholder text
+     */
+    placeholder          : PropTypes.string,
+    /**
+     *  HTML attribute controlling input spell check
+     */
+    spellCheck           : PropTypes.bool,
+    /**
+     *  Input text alignment
+     */
+    textAlign            : PropTypes.oneOf( [ 'auto', 'left', 'right' ] ),
+    /**
+     *  Input string value
+     */
+    value                : PropTypes.string,
+};
+
+TextInputWithIcon.defaultProps =
+{
+    aria                 : undefined,
+    autoCapitalize       : undefined,
+    autoComplete         : undefined,
+    autoCorrect          : undefined,
+    className            : undefined,
+    cssMap               : undefined,
+    forceHover           : false,
+    hasError             : false,
+    iconButtonIsDisabled : false,
+    iconPosition         : 'right',
+    iconType             : 'none',
+    id                   : undefined,
+    inputRef             : undefined,
+    inputType            : 'text',
+    isDisabled           : false,
+    isReadOnly           : false,
+    name                 : undefined,
+    onBlur               : undefined,
+    onChangeInput        : undefined,
+    onClick              : undefined,
+    onClickIcon          : undefined,
+    onFocus              : undefined,
+    onKeyDownInput       : undefined,
+    onMouseOut           : undefined,
+    onMouseOver          : undefined,
+    placeholder          : undefined,
+    spellCheck           : undefined,
+    textAlign            : 'auto',
+    value                : '',
+};
+
+TextInputWithIcon.displayName = componentName;
+
+export default TextInputWithIcon;
