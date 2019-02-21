779a034a0dcc07479c6c5413a0458eeec6f41882
Daniel Martic <daniel.martic@sociomantic.com>
2019-02-21 17:34:10 +0100

    Passed componentName to generateId #885

--- src/DatePicker/TimeInput.jsx
+++ src/DatePicker/TimeInput.jsx
@@ -24,7 +24,7 @@ const TimeInput = props =>
         hourIsReadOnly,
         hourPlaceholder,
         hourValue,
-        id = generateId( 'TimeInput' ),
+        id = generateId( componentName ),
         isDisabled,
         isReadOnly,
         minuteIsDisabled,
